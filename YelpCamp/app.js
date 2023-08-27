1a5bfb57f80d9a83bc01674597cd35dcf05c9acb
tsimeonov <tsimeonov12@gmail.com>
2023-08-27 16:24:33 +0200

    creating the mongoose starter code

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,4 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
 const app = express();
 const path = require("path");
 
