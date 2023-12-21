a8337c327a6b6dbe64c84a566e5116b9cfd12be6
tsimeonov <tsimeonov12@gmail.com>
2023-12-21 18:33:22 +0100

    egg timer code

--- iOS & Swift Bootcamp/Section 8 Intermediate Swift Programming/EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ iOS & Swift Bootcamp/Section 8 Intermediate Swift Programming/EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -2,14 +2,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    @IBOutlet weak var titleLabel: UILabel!
+    let eggTimer = ["Soft": 3, "Medium": 4, "Hard": 7]
+    
+    var secondsRemainig = 60
+    
+    var timer = Timer()
     
     @IBAction func hardnessSelected (_ sender: UIButton) {
         
-        let hardness = sender.currentTitle
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle! // Soft, Medium, Hard
+        
+        secondsRemainig = eggTimer[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
-
+    @objc func updateTimer() {
+        if secondsRemainig > 0 {
+            print("\(secondsRemainig) seconds")
+            secondsRemainig -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Done"
+        }
+    }
 }
--- iOS & Swift Bootcamp/Section 8 Intermediate Swift Programming/section_8.md
+++ iOS & Swift Bootcamp/Section 8 Intermediate Swift Programming/section_8.md
@@ -83,3 +83,41 @@ print(player1Username?) // Output jackbaur
 ---
 
 <h5>Egg timer</h5>
+
+```swift
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    let eggTimer = ["Soft": 3, "Medium": 4, "Hard": 7]
+
+    var secondsRemainig = 60
+
+    var timer = Timer()
+
+    @IBAction func hardnessSelected (_ sender: UIButton) {
+
+        timer.invalidate()
+
+        let hardness = sender.currentTitle! // Soft, Medium, Hard
+
+        secondsRemainig = eggTimer[hardness]!
+
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+
+    @objc func updateTimer() {
+        if secondsRemainig > 0 {
+            print("\(secondsRemainig) seconds")
+            secondsRemainig -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Done"
+        }
+    }
+}
+
+```
+
+---
