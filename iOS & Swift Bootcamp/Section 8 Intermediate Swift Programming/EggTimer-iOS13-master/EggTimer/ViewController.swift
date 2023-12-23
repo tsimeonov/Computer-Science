df47270408923a92efb23df77cc02b5ef45b6d8a
tsimeonov <tsimeonov12@gmail.com>
2023-12-23 14:35:40 +0100

    new folder

--- iOS & Swift Bootcamp/Section 8 Intermediate Swift Programming/EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ iOS & Swift Bootcamp/Section 8 Intermediate Swift Programming/EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -17,6 +17,10 @@ class ViewController: UIViewController {
         
         totalTime = eggTimer[hardness]!
         
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
     }
@@ -24,11 +28,9 @@ class ViewController: UIViewController {
     @objc func updateTimer() {
         if secondsPassed < totalTime{
             
-            let percentageProgess = secondsPassed / totalTime
-            
-            progressBar.progress = Float(percentageProgess)
-            
             secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            
         } else {
             timer.invalidate()
             titleLabel.text = "Done"
