b837c98ef55c1658dcf1ce1a4c907f392f6d6ea8
tsimeonov <tsimeonov12@gmail.com>
2024-01-07 16:45:27 +0100

    paste the code

--- iOS & Swift Bootcamp/Section 7 Apple documentation/Xylophone-iOS13-master/Xylophone/ViewController.swift
+++ iOS & Swift Bootcamp/Section 7 Apple documentation/Xylophone-iOS13-master/Xylophone/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,27 @@ class ViewController: UIViewController {
         print("I got pressed")
     }
     
-    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 
 }
 
--- iOS & Swift Bootcamp/Section 7 Apple documentation/step_by_step.md
+++ iOS & Swift Bootcamp/Section 7 Apple documentation/step_by_step.md
@@ -21,5 +21,31 @@ import AVFoundation
 - Create a variable `player` and specify the data type `AVAudioPlayer`
 
 ```swift
-var player: AVAudioPlayer
+var player: AVAudioPlayer?
+```
+
+- Paste the code in the project
+
+```swift
+func playSound() {
+    guard let url = Bundle.main.url(forResource: "soundName", withExtension: "mp3") else { return }
+
+    do {
+        try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+        try AVAudioSession.sharedInstance().setActive(true)
+
+        /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+        player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+        /* iOS 10 and earlier require the following line:
+        player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+        guard let player = player else { return }
+
+        player.play()
+
+    } catch let error {
+        print(error.localizedDescription)
+    }
+}
 ```
