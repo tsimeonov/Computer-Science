07ce8f02b8cdfd553932fa115b2d5402ce19906a
tsimeonov <tsimeonov12@gmail.com>
2024-05-05 17:37:40 +0200

    add a new file

--- React course/01_tutorial/src/index.js
+++ React course/01_tutorial/src/index.js
@@ -4,12 +4,19 @@ import ReactDom from "react-dom";
 // CSS
 import "./index.css";
 
-// Setup Variabled
-const firstBook = {
-  img: "https://m.media-amazon.com/images/I/71I3CatXc8L._AC._SR360,460.jpg",
-  title: "Butter: The Cult new Japanese Bestselling Novel",
-  author: "Asako Yuzuki",
-};
+// Setup Variables
+const books = [
+  {
+    img: "https://m.media-amazon.com/images/I/71I3CatXc8L._AC._SR360,460.jpg",
+    title: "Butter: The Cult new Japanese Bestselling Novel",
+    author: "Asako Yuzuki",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/71UUUzTDmSL._AC._SR360,460.jpg",
+    title: "On the Origin of Time: The instant Sunday Times bestseller",
+    author: "Thomas Hertog",
+  },
+];
 
 function BookList() {
   return (
@@ -18,25 +25,22 @@ function BookList() {
         img={firstBook.img}
         title={firstBook.title}
         author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio
-          id dolores delectus voluptatibus aspernatur, at dicta fuga ex culpa
-          recusandae.
-        </p>
-      </Book>
+      ></Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      ></Book>
     </section>
   );
 }
 
-const Book = ({ img, title, author, children }) => {
-  // const { img, title, author } = props;
+const Book = ({ img, title, author }) => {
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
