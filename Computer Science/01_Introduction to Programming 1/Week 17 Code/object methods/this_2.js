058abe762b2ba74316d74a640d79057907dcb1ad
tsimeonov <tsimeonov12@gmail.com>
2024-08-12 20:17:07 +0200

    update the code

--- Computer Science/01_Introduction to Programming 1/Week 17 Code/object methods/this_2.js
+++ Computer Science/01_Introduction to Programming 1/Week 17 Code/object methods/this_2.js
@@ -1,78 +1,90 @@
-var rocket; 
+var rocket;
 var baseLine;
 
 function setup()
 {
      createCanvas(800, 600);
-    
+
     baseLine = height - 100
 
      rocket =    {
         x: width/2,
-        y: baseLine, 
-        thrust: false, 
+        y: baseLine,
+        thrust: false,
         moveLeft: false,
-        moveRight: false
+        moveRight: false,
+		drawRocket: function() {
+			//draw the rocket
+			fill(180)
+			beginShape();
+			vertex(this.x + 10, this.y + 60);
+			vertex(this.x + 10, this.y + 20);
+			vertex(this.x + 15, this.y);
+			vertex(this.x + 20, this.y + 20);
+			vertex(this.x + 20, this.y + 60);
+			endShape(CLOSE);
+
+			fill(255, 0, 0);
+			beginShape();
+			vertex(this.x, this.y + 60);
+			vertex(this.x + 10, this.y + 40);
+			vertex(this.x + 10, this.y + 60);
+			endShape(CLOSE);
+
+			beginShape();
+			vertex(this.x + 30, this.y + 60);
+			vertex(this.x + 20, this.y + 40);
+			vertex(this.x + 20, this.y + 60);
+			endShape(CLOSE);
+
+			if (this.thrust)
+				{
+					fill(255, 150, 0);
+					beginShape();
+					vertex(this.x + 10, this.y + 60);
+					vertex(this.x + 13, this.y + 80);
+					vertex(this.x + 15, this.y + 70);
+					vertex(this.x + 18, this.y + 80);
+					vertex(this.x + 20, this.y + 60);
+					endShape(CLOSE);
+				}
+
+		},
+		moveRocket: function () {
+		if (this.thrust && this.y > 0)
+		{
+			this.y -= 2;
+		}
+		else if (this.y < baseLine)
+		{
+			this.y += 3;
+		}
+
+		if (this.moveLeft && this.x > 0 && this.y != baseLine)
+		{
+			this.x -= 2;
+		}
+
+		if (this.moveRight && this.x < width && this.y != baseLine)
+		{
+			this.x += 2;
+		}
+		}
     };
-    
+
 }
 function draw()
 {
     //move the rocket
-    if (rocket.thrust && rocket.y > 0)
-	{
-		rocket.y -= 2;
-	}
-	else if (rocket.y < baseLine)
-	{
-		rocket.y += 3;
-	}
+	rocket.moveRocket()
 
-	if (rocket.moveLeft && rocket.x > 0 && rocket.y != baseLine)
-	{
-		rocket.x -= 2;
-	}
 
-	if (rocket.moveRight && rocket.x < width && rocket.y != baseLine)
-	{
-		rocket.x += 2;
-	}
-    
     background(10);
-  //draw the rocket
-    fill(180)
-beginShape();
-	vertex(rocket.x + 10, rocket.y + 60);
-	vertex(rocket.x + 10, rocket.y + 20);
-	vertex(rocket.x + 15, rocket.y);
-	vertex(rocket.x + 20, rocket.y + 20);
-	vertex(rocket.x + 20, rocket.y + 60);
-	endShape(CLOSE);
 
-	fill(255, 0, 0);
-	beginShape();
-	vertex(rocket.x, rocket.y + 60);
-	vertex(rocket.x + 10, rocket.y + 40);
-	vertex(rocket.x + 10, rocket.y + 60);
-	endShape(CLOSE);
+	// Draw the rocket
+	rocket.drawRocket()
 
-	beginShape();
-	vertex(rocket.x + 30, rocket.y + 60);
-	vertex(rocket.x + 20, rocket.y + 40);
-	vertex(rocket.x + 20, rocket.y + 60);
-	endShape(CLOSE);
 
-	if (rocket.thrust)
-	{
-		fill(255, 150, 0);
-		beginShape();
-		vertex(rocket.x + 10, rocket.y + 60);
-		vertex(rocket.x + 13, rocket.y + 80);
-		vertex(rocket.x + 15, rocket.y + 70);
-		vertex(rocket.x + 18, rocket.y + 80);
-		vertex(rocket.x + 20, rocket.y + 60);
-		endShape(CLOSE);
-	}
 }
 
 function keyPressed()
@@ -99,12 +111,12 @@ function keyReleased()
     {
 	   rocket.thrust = false;
     }
-    
+
     if(key == "A")
     {
 	   rocket.moveLeft = false;
     }
-    
+
     if(key == "D")
     {
 	   rocket.moveRight = false;
