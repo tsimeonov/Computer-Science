0dcbebe6df9c511374ee74e4ca6845d5110c744e
tsimeonov <tsimeonov12@gmail.com>
2024-08-12 20:01:36 +0200

    add another file

---
+++ Computer Science/01_Introduction to Programming 1/Week 17 Code/object methods/this_1.js
@@ -0,0 +1,17 @@
+var myRect;
+
+function setup()
+{
+    createCanvas(800, 600);
+    myRect = {
+        x: 100, 
+        y: 100, 
+        width: 200,
+        height: 300
+    }
+}
+
+function draw()
+{
+    
+}
