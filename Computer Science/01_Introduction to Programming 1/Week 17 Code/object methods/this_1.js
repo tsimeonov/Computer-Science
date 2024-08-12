2c6f9cef2a85fb5fcbfa20334dc67056f981986d
tsimeonov <tsimeonov12@gmail.com>
2024-08-12 20:04:56 +0200

    update hte code

--- Computer Science/01_Introduction to Programming 1/Week 17 Code/object methods/this_1.js
+++ Computer Science/01_Introduction to Programming 1/Week 17 Code/object methods/this_1.js
@@ -4,14 +4,18 @@ function setup()
 {
     createCanvas(800, 600);
     myRect = {
-        x: 100, 
-        y: 100, 
+        x: 100,
+        y: 100,
         width: 200,
-        height: 300
+        height: 300,
+        drawRect: function() {
+            rect(this.x, this.y, this.width, this.height)
+        }
     }
 }
 
 function draw()
 {
-    
+    fill(0,200,0)
+    myRect.drawRect()
 }
