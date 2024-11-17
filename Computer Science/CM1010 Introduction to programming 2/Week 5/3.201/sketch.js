050619d718b9b867ccb0272c7b73855182809d5a
tsimeonov <tsimeonov12@gmail.com>
2024-11-17 20:27:37 +0100

    add the example code

---
+++ Computer Science/CM1010 Introduction to programming 2/Week 5/3.201/sketch.js
@@ -0,0 +1,36 @@
+/*
+
+- Copy your game project code into this file
+- for the p5.Sound library look here https://p5js.org/reference/#/libraries/p5.sound
+- for finding cool sounds perhaps look here
+https://freesound.org/
+
+
+*/
+
+
+
+var jumpSound;
+
+function preload()
+{
+    soundFormats('mp3','wav');
+
+    //load your sounds here
+    jumpSound = loadSound('assets/segway_loop.mp3');
+    jumpSound.setVolume(0.1);
+}
+
+
+function setup()
+{
+	createCanvas(1024, 576);
+
+}
+
+
+function keyPressed()
+{
+    //jumpSound.play();
+    jumpSound.loop(0,0.5, 0.1,0.1);
+}
