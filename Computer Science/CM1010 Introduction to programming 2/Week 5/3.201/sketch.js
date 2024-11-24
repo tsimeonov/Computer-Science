45d5ee8f0cc722174fe7c32cef6daea267807728
tsimeonov <tsimeonov12@gmail.com>
2024-11-24 09:29:03 +0100

    add a comment

--- Computer Science/CM1010 Introduction to programming 2/Week 5/3.201/sketch.js
+++ Computer Science/CM1010 Introduction to programming 2/Week 5/3.201/sketch.js
@@ -38,6 +38,7 @@ function keyPressed() {
     isInitialised = true;
     jumpSound.setVolume(0.2);
     //jumpSound.play();
+    // Use the r variable to adjust the sound speed
     var r = map(mouseX, 0, width, 0.4, 4);
     jumpSound.loop(0, r);
   }
