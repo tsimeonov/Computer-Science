7b7dfc1293b6076579788912135adf22745d76b2
tsimeonov <tsimeonov12@gmail.com>
2025-01-08 19:58:22 +0100

    rename the folder

--- Javascript/The Complete JavaScript Course /Section 9 Data Structures, Modern Operators and Strings/app.js
+++ Javascript/The Complete JavaScript Course /Section 9 Data Structures, Modern Operators and Strings/app.js
@@ -47,32 +47,14 @@ const restaurant = {
   },
 };
 
-const rest1 = {
-  name: "Capri",
-  // numGuest: 20,
-  numGuest: 0,
-};
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
-const rest2 = {
-  name: "La piazza",
-  owner: "Giovanni Rossi",
-};
+// Select the element
+for (const item of menu) console.log(item);
+// Focaccia, Bruschetta, Garlic Bread ,Caprese Salad, Pizza, Pasta, Risotto
 
-// rest1.numGuest = rest1.numGuest || 10;
-// rest1.numGuest ||= 10;
-// Nullish assignment operator
-rest1.numGuest ??= 10;
+for (const item of menu.entries()) {
+  console.log(item);
+}
 
-// rest2.numGuest = rest2.numGuest || 10;
-// rest2.numGuest ||= 10;
-// Nullish assignment operator
-rest2.numGuest ??= 10;
-
-// AND assignment operator
-// rest1.owner = rest1.owner && "<ANONYMOUS>";
-// rest2.owner = rest2.owner && "<ANONYMOUS>";
-rest1.owner &&= "<ANONYMOUS>";
-rest2.owner &&= "<ANONYMOUS>";
-
-console.log(rest1);
-console.log(rest2);
+console.log(menu.entries());
