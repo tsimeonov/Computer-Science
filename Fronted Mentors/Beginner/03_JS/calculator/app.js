6c19f872422a0b6d008e4a938e7f462c7bcbaaa3
tsimeonov <tsimeonov12@gmail.com>
2025-03-22 16:19:47 +0100

    add function buttonClick

--- Fronted Mentors/Beginner/03_JS/calculator/app.js
+++ Fronted Mentors/Beginner/03_JS/calculator/app.js
@@ -2,3 +2,12 @@ let runningTotal = 0;
 let buffer = "0";
 let previousOperator;
 const screen = document.querySelector(".screen");
+
+function buttonClick(value) {
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  ReadableStreamDefaultController();
+}
