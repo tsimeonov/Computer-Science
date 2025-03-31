cf788eec486159b1e3a68d5f52cf377730100366
tsimeonov <tsimeonov12@gmail.com>
2025-03-31 16:13:10 +0200

    Optional Chaining

--- Javascript/The Complete JavaScript Course /Section 9 Data Structures, Modern Operators and Strings/app.js
+++ Javascript/The Complete JavaScript Course /Section 9 Data Structures, Modern Operators and Strings/app.js
@@ -4,14 +4,7 @@
 const flights =
   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
-// Data needed for first part of the section
-const restaurant = {
-  name: "Classico Italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto"],
-
+const openingHours = {
   openingHours: {
     thu: {
       open: 12,
@@ -26,33 +19,30 @@ const restaurant = {
       close: 24,
     },
   },
+};
 
-  order: function (starterIndex, mainIndex) {
+// Data needed for first part of the section
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  // ES6 enhanced object literals
+  openingHours,
+
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({
-    startIndex = 1,
-    mainIndex = 0,
-    time = "20.00",
-    address,
-  }) {
+  orderDelivery({ startIndex = 1, mainIndex = 0, time = "20.00", address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainIndex[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
   },
 };
-
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-
-for (const item of menu) console.log(item);
-
-// Rename element 0 to element 1
-
-for (const [i, el] of menu.entries()) {
-  console.log(`${i + 1}: ${el}`);
-}
