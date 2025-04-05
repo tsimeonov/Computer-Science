01f6bfe7cd0586b34fa51e0a96b3b6f32c70ba2b
tsimeonov <tsimeonov12@gmail.com>
2025-04-05 16:15:17 +0200

    add the object

--- Javascript/The Complete JavaScript Course /Section 9 Data Structures, Modern Operators and Strings/Challenge #1.js
+++ Javascript/The Complete JavaScript Course /Section 9 Data Structures, Modern Operators and Strings/Challenge #1.js
@@ -2,9 +2,52 @@
 
 // We're building a football betting app (soccer for my American friends 😅)!
 
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
 // Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
 
 // 1. Create one player array for each team (variables 'players1' and 'players2')
+
+const [players1, players2] = game.players;
 
 // 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
 
