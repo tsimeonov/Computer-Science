75893ab5988c09aa45bb06e612aa26e65e3d3077
tsimeonov <tsimeonov12@gmail.com>
2025-04-05 16:55:37 +0200

    problem 6

--- Javascript/The Complete JavaScript Course /Section 9 Data Structures, Modern Operators and Strings/Challenge #1.js
+++ Javascript/The Complete JavaScript Course /Section 9 Data Structures, Modern Operators and Strings/Challenge #1.js
@@ -75,6 +75,15 @@ console.log(team1, draw, team2);
 
 // 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
 
+const printGoals = function (...players) {
+  console.log(players);
+  console.log(`${players.length} goals were scored`);
+};
+
+console.log("Davies", "Muller", "Lewandowski", "Kimmich");
+console.log("Davies", "Muller");
+printGoals(...game.scored);
+
 // 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
 
 // TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
