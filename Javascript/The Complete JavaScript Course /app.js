b7c84305e15a753b2c33e6fae2eee2e89b1761ae
tsimeonov <tsimeonov12@gmail.com>
2025-07-11 16:44:06 +0200

    Update app.js

--- Javascript/The Complete JavaScript Course /app.js
+++ Javascript/The Complete JavaScript Course /app.js
@@ -1,34 +1,27 @@
-// Data needed for a later exercise
-const flights =
-  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+'use strict';
 
-// Data needed for first part of the section
-const restaurant = {
-  name: "Classico Italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto"],
+const bookings = [];
 
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
+const createBooking = function (
+	flightNum,
+	numPassengers = 1,
+	price = 199 * numPassengers
+) {
+	// ES5
+	// numPassengers = numPassengers || 1;
+	// price = price || 199;
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+	const booking = {
+		flightNum,
+		numPassengers,
+		price,
+	};
+
+	console.log(booking);
+
+	bookings.push(booking);
 };
 
-const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r); // 8 9 1
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 5);
