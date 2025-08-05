fc99a6c63df18640eeb6ec0030b54dc8567ebed5
tsimeonov <tsimeonov12@gmail.com>
2025-08-05 20:03:37 +0200

    update loops

--- Javascript/The Complete JavaScript Course /Section 11 Arrays/01_forEach.js
+++ Javascript/The Complete JavaScript Course /Section 11 Arrays/01_forEach.js
@@ -4,21 +4,21 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // FOR OF
 console.log('FOR OF');
-for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
 	if (movement > 0) {
-		console.log(`You deposited ${movement}`);
+		console.log(`Movement ${i + 1} You deposited ${movement}`);
 	} else {
-		console.log(`You withdre ${movement}`);
+		console.log(`Movement ${i + 1} You withdre ${movement}`);
 	}
 }
 
 // FOR EACH
 console.log('FOR EACH');
-movements.forEach(function (movement) {
+movements.forEach(function (movement, i, arr) {
 	if (movement > 0) {
-		console.log(`You deposited ${movement}`);
+		console.log(`Movement ${i + 1} You deposited ${movement}`);
 	} else {
-		console.log(`You withdre ${movement}`);
+		console.log(`Movement ${i + 1} You withdre ${movement}`);
 	}
 });
 
