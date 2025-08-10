bb8b8867d72fae9f0cf72b7fea75cc954ae3025b
tsimeonov <tsimeonov12@gmail.com>
2025-08-10 13:20:20 +0200

    add code

--- Javascript/The Complete JavaScript Course /Starter code/11-Arrays-Bankist/starter/script.js
+++ Javascript/The Complete JavaScript Course /Starter code/11-Arrays-Bankist/starter/script.js
@@ -82,6 +82,24 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 // displayMovements(account1.movements);
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+     <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__date">3 days ago</div>
+      <div class="movements__value">${mov}</div>
+    </div>
+  `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
 displayMovements(account1.movements);
 
 /////////////////////////////////////////////////
