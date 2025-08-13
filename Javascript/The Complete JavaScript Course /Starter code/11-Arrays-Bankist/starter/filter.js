343d9b078bdb17c9f82f34712245af0bf66d16b1
tsimeonov <tsimeonov12@gmail.com>
2025-08-13 19:48:12 +0200

    Update filter.js

--- Javascript/The Complete JavaScript Course /Starter code/11-Arrays-Bankist/starter/filter.js
+++ Javascript/The Complete JavaScript Course /Starter code/11-Arrays-Bankist/starter/filter.js
@@ -1,10 +1,10 @@
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+console.log(movements);
 
 const deposits = movements.filter(function (mov) {
   return mov > 0;
 });
 
-console.log(movements);
 console.log(deposits);
 
 const depositsFor = [];
