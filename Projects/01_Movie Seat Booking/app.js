6ab86df277ec399b7193793a4bf9ce3842aa66c5
tsimeonov <tsimeonov12@gmail.com>
2025-08-16 14:15:13 +0200

    define variables

--- Projects/01_Movie Seat Booking/app.js
+++ Projects/01_Movie Seat Booking/app.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.rwo .seat:not(.occupied)');
+const count = document.querySelector('#count');
+const total = document.querySelector('#total');
+const movie = document.querySelector('#movie');
