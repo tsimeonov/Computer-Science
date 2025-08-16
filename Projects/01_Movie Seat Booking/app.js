07638c55fa42f45e54b5dde0247f79811007f38a
tsimeonov <tsimeonov12@gmail.com>
2025-08-16 14:16:36 +0200

    Update app.js

--- Projects/01_Movie Seat Booking/app.js
+++ Projects/01_Movie Seat Booking/app.js
@@ -4,4 +4,9 @@ const container = document.querySelector('.container');
 const seats = document.querySelectorAll('.rwo .seat:not(.occupied)');
 const count = document.querySelector('#count');
 const total = document.querySelector('#total');
-const movie = document.querySelector('#movie');
+const movieSelect = document.querySelector('#movie');
+
+// populateUI();
+
+// +movieSelect.value -> unary operator
+let ticketPrice = +movieSelect.value;
