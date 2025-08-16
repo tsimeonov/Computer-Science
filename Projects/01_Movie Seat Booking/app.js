8df649a5bbea878b513f7bafa97095d7776b478e
tsimeonov <tsimeonov12@gmail.com>
2025-08-16 14:30:05 +0200

    create populateUI()

--- Projects/01_Movie Seat Booking/app.js
+++ Projects/01_Movie Seat Booking/app.js
@@ -32,3 +32,22 @@ function UpdateSelectedCount() {
 
 	setMovieData(movieSelect.selectedIndex, movieSelect.value);
 }
+
+// Get data from localStorage and populate UI
+function populateUI() {
+	const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+	if (selectedSeats !== null && selectedSeats.length > 0) {
+		seats.forEach((seat, index) => {
+			if (selectedSeats.indexOf(index) > -1) {
+				seat.classList.add('selected');
+			}
+		});
+	}
+
+	const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+	if (selectedMovieIndex !== null) {
+		movieSelect.selectedIndex = selectedMovieIndex;
+	}
+}
