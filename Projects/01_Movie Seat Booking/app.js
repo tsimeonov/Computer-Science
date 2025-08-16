893e26567d19aea694e0a8398abbe4533cabb771
tsimeonov <tsimeonov12@gmail.com>
2025-08-16 14:32:58 +0200

    add eventListener

--- Projects/01_Movie Seat Booking/app.js
+++ Projects/01_Movie Seat Booking/app.js
@@ -51,3 +51,15 @@ function populateUI() {
 		movieSelect.selectedIndex = selectedMovieIndex;
 	}
 }
+
+// Movie click event
+container.addEventListener('click', (e) => {
+	if (
+		e.target.classList.contains('seat') &&
+		!e.target.classList.contains('occupied')
+	) {
+		e.target.classList.toggle('selected');
+
+		UpdateSelectedCount();
+	}
+});
