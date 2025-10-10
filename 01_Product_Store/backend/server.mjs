ef2a33345893fc6ef14ad6a26343495c98b00dda
tsimeonov <tsimeonov12@gmail.com>
2025-10-10 15:38:40 +0200

    update

--- 01_Product_Store/backend/server.mjs
+++ 01_Product_Store/backend/server.mjs
@@ -4,6 +4,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+	res.send('Server is ready');
+});
+
 app.listen(5000, () => {
-	console.log('Server started at locolhost:5000');
+	console.log('Server started at http://localhost:6000');
 });
