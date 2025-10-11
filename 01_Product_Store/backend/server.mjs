54db767761f64c8c051a7aa5c5054304cd7ca6bd
tsimeonov <tsimeonov12@gmail.com>
2025-10-11 15:16:19 +0200

    update

--- 01_Product_Store/backend/server.mjs
+++ 01_Product_Store/backend/server.mjs
@@ -1,12 +1,17 @@
 // const express = require("express")
 
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.get('/', (req, res) => {
 	res.send('Server is ready');
 });
+
+console.log(process.env.MONGO_URI);
 
 app.listen(5000, () => {
 	console.log('Server started at http://localhost:5000');
---
+++ 01_Product_Store/config/db.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+
+export const connectBD = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI);
+		console.log(`MongoDB connected: ${conn.connection.host}`);
+	} catch (error) {}
+};
