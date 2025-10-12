538c614c1f526c7e0d58bb6bc47d6392520053ab
tsimeonov <tsimeonov12@gmail.com>
2025-10-12 12:56:06 +0200

    delete function

--- 01_Product_Store/backend/server.mjs
+++ 01_Product_Store/backend/server.mjs
@@ -31,6 +31,17 @@ app.post('/api/products', async (req, res) => {
 	}
 });
 
+app.delete('/api/products/:id', async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		await Product.findByIdAndDelete(id);
+		res.status(200).json({ success: true, message: 'Product deleted' });
+	} catch (error) {
+		res.status(404).json({ success: false, message: 'Product not found' });
+	}
+});
+
 // console.log(process.env.MONGO_URI);
 
 app.listen(5000, () => {
