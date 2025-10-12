6fb6948a4c8c502be5f3fbf0c3e40285da4c8fc7
tsimeonov <tsimeonov12@gmail.com>
2025-10-12 13:51:14 +0200

    refactor

---
+++ 01_Product_Store/routes/product.route.js
@@ -0,0 +1,68 @@
+import express from 'express';
+
+const router = express.router();
+
+router.get('/api/products', async (req, res) => {
+	try {
+		const products = await Product.find({});
+		res.status(200).json({ success: true, data: products });
+	} catch (error) {
+		console.log('error in fetching products', error.message);
+		res.status(500).json({ success: false, message: 'Server error' });
+	}
+});
+
+router.post('/api/products', async (req, res) => {
+	const product = req.body; // User will end the data
+
+	if (!product.name || !product.price || !product.image) {
+		return res
+			.status(400)
+			.json({ success: false, message: 'Please provide all fields' });
+	}
+
+	const newProduct = new Product(product);
+
+	try {
+		await newProduct.save();
+		res.status(201).json({ success: true, data: newProduct });
+	} catch (error) {
+		console.error('Error in Create product:', error.message);
+		res.status(500).json({ success: false, message: 'Server error' });
+	}
+});
+
+router.put('/api/products/:id', async (req, res) => {
+	const { id } = req.params;
+
+	const product = req.body;
+
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res
+			.status(404)
+			.json({ success: false, message: 'Invalid Product id' });
+	}
+
+	try {
+		const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+			new: true,
+		});
+		res.status(200).json({ success: true, data: updatedProduct });
+	} catch (error) {
+		res.status(500).json({ success: false, message: 'Server error' });
+	}
+});
+
+router.delete('/api/products/:id', async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		await Product.findByIdAndDelete(id);
+		res.status(200).json({ success: true, message: 'Product deleted' });
+	} catch (error) {
+		console.log('Error in deleting product:', error.message);
+		res.status(404).json({ success: false, message: 'Product not found' });
+	}
+});
+
+export default router;
