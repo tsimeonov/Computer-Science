5a2ba2d903aed0d15cfc456476859c1ac7f06dfb
tsimeonov <tsimeonov12@gmail.com>
2025-10-12 13:59:40 +0200

    add files

--- 01_Product_Store/routes/product.route.js
+++ 01_Product_Store/routes/product.route.js
@@ -4,16 +4,6 @@ import Product from '../models/product.model.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-	try {
-		const products = await Product.find({});
-		res.status(200).json({ success: true, data: products });
-	} catch (error) {
-		console.log('error in fetching products', error.message);
-		res.status(500).json({ success: false, message: 'Server error' });
-	}
-});
-
 router.post('/', async (req, res) => {
 	const product = req.body; // User will end the data
 
