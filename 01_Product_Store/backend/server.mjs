69603e83179397949559ece908dc3fa28427a8c6
tsimeonov <tsimeonov12@gmail.com>
2025-10-18 15:55:26 +0200

    add the port

--- 01_Product_Store/backend/server.mjs
+++ 01_Product_Store/backend/server.mjs
@@ -9,6 +9,7 @@ import productRoutes from '../routes/product.route.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to accept JSON data in req.body
 
@@ -18,5 +19,5 @@ app.use('/api/products', productRoutes);
 
 app.listen(5000, () => {
 	connectBD();
-	console.log('Server started at http://localhost:5000');
+	console.log(`Server started at http://localhost:${PORT}`);
 });
