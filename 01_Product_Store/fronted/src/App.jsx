dae65458474b92c502e260c20069ca21435fa1d5
tsimeonov <tsimeonov12@gmail.com>
2025-10-19 15:08:56 +0200

    add Routes

--- 01_Product_Store/fronted/src/App.jsx
+++ 01_Product_Store/fronted/src/App.jsx
@@ -1,7 +1,14 @@
-import { Button } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
 
 function App() {
-	return <></>;
+	return (
+		<Box minH={'100vh'}>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/create" element={<CreatePage />} />
+			</Routes>
+		</Box>
+	);
 }
 
 export default App;
