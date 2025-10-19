cc5797c24839b07769d6e51a5473e1af0d24da66
tsimeonov <tsimeonov12@gmail.com>
2025-10-19 15:16:46 +0200

    import Navbar

--- 01_Product_Store/fronted/src/App.jsx
+++ 01_Product_Store/fronted/src/App.jsx
@@ -2,10 +2,12 @@ import { Box, Button } from '@chakra-ui/react';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CreatePage from './pages/CreatePage';
+import Navbar from './component/Navbar';
 
 function App() {
 	return (
 		<Box minH={'100vh'}>
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/create" element={<CreatePage />} />
