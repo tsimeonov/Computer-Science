fba19ac792808833e0f67a6ddcd107dea0e35fbc
tsimeonov <tsimeonov12@gmail.com>
2025-10-21 19:48:30 +0200

    add container

--- 01_Product_Store/fronted/src/pages/CreatePage.jsx
+++ 01_Product_Store/fronted/src/pages/CreatePage.jsx
@@ -1,10 +1,17 @@
+import { Box, useColorModeValue, VStack } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router-dom';
+
 const CreatePage = () => {
 	const [newProduct, setNewProduct] = useState({
 		name: '',
 		price: '',
 		image: '',
 	});
-	return <div>CreatePage</div>;
+	return (
+		<Container maxW={'container.sm'}>
+			<VStack spacing={8}></VStack>
+		</Container>
+	);
 };
 
 export default CreatePage;
