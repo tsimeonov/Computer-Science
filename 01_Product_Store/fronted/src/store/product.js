4f71c14c81b2cb93dcadcbdab6fd85fe7ad1ca16
tsimeonov <tsimeonov12@gmail.com>
2025-10-22 19:53:51 +0200

    add a server

--- 01_Product_Store/fronted/src/store/product.js
+++ 01_Product_Store/fronted/src/store/product.js
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import { create } from 'zustand';
 
 export const useProductStore = create((set) => ({
 	products: [],
 	setProducts: (products) => set({ products }),
+	createProduct: async (newProduct) => {
+		if (!newProduct.name || !newProduct.image || !newProduct.price) {
+			return { success: false, message: 'Please fill in all fields' };
+		}
+		const res = await fetch('hppt//localhost:5000/api/products');
+	},
 }));
