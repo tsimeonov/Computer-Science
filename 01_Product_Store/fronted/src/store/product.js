1cd149dce27ac9901997413db6695ae0d663e041
tsimeonov <tsimeonov12@gmail.com>
2025-10-22 19:57:53 +0200

    complete teh code

--- 01_Product_Store/fronted/src/store/product.js
+++ 01_Product_Store/fronted/src/store/product.js
@@ -8,6 +8,14 @@ export const useProductStore = create((set) => ({
 		if (!newProduct.name || !newProduct.image || !newProduct.price) {
 			return { success: false, message: 'Please fill in all fields' };
 		}
-		const res = await fetch('hppt//localhost:5000/api/products');
+		const res = await fetch('/api/products', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(newProduct),
+		});
+		const data = await res.json();
+		set((state) => ({ products: [...state.products, data] }));
 	},
 }));
