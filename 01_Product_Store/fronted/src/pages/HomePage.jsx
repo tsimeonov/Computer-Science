e92700f36a226868ccd3fce83736782c1ebabf32
tsimeonov <tsimeonov12@gmail.com>
2025-10-26 13:55:43 +0100

    update

--- 01_Product_Store/fronted/src/pages/HomePage.jsx
+++ 01_Product_Store/fronted/src/pages/HomePage.jsx
@@ -2,8 +2,17 @@ import { Container, Text, VStack } from '@chakra-ui/react';
 
 const HomePage = () => {
 	return (
-		<Container maxw="container.xl" py={12}>
-			<VStack spacing={8}></VStack>
+		<Container maxW="container.xl" py={12}>
+			<VStack spacing={8}>
+				<Text
+					fontSize={'3xl'}
+					fontWeight={'bold'}
+					bgGradient={'linear(to-r, cyan.400, blue.500)'}
+					bgClip={'text'}
+					textAlign={'center'}>
+					Current Products
+				</Text>
+			</VStack>
 		</Container>
 	);
 };
