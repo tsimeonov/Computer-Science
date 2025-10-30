82bb40e9d54cb689b55b01597f45a7faa59b4548
tsimeonov <tsimeonov12@gmail.com>
2025-10-30 19:18:59 +0100

    add return

--- 01_Product_Store/fronted/src/store/product.js
+++ 01_Product_Store/fronted/src/store/product.js
@@ -33,5 +33,6 @@ export const useProductStore = create((set) => ({
 		set((state) => ({
 			products: state.products.filter((product) => product._id !== pid),
 		}));
+		return { succes: true, message: data.message };
 	},
 }));
