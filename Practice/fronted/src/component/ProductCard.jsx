bf1dc427eff3e5a2852bd0443060235021c90816
tsimeonov <tsimeonov12@gmail.com>
2025-11-16 13:13:08 +0100

    update

--- Practice/fronted/src/component/ProductCard.jsx
+++ Practice/fronted/src/component/ProductCard.jsx
@@ -1,4 +1,3 @@
-// import { DeleteIcon, EditIcon } from '@chakra-ui/icons';
 import {
 	Box,
 	Button,
@@ -80,99 +79,13 @@ const ProductCard = ({ product }) => {
 	};
 
 	return (
-		<Box
-			shadow="lg"
-			rounded="lg"
-			overflow="hidden"
-			transition="all 0.3s"
-			_hover={{ transform: 'translateY(-5px)', shadow: 'xl' }}
-			bg={bg}>
-			<Image
-				src={product.image}
-				alt={product.name}
-				h={48}
-				w="full"
-				objectFit="contain"
-			/>
-
-			<Box p={4}>
-				<Heading as="h3" size="md" mb={2}>
-					{product.name}
-				</Heading>
-
-				<Text fontWeight="bold" fontSize="xl" color={textColor} mb={4}>
-					${product.price}
-				</Text>
-
-				<HStack spacing={2}>
-					<IconButton
-						icon={<BiEditAlt />}
-						onClick={onOpen}
-						colorScheme="blue"
-					/>
-					<IconButton
-						icon={<AiFillDelete />}
-						onClick={() => handleDeleteProduct(product._id)}
-						colorScheme="red"
-					/>
-				</HStack>
+		<Box>
+			<Box>
+				<Image src={product.image} />
+				<Heading>{product.name}</Heading>
+				<Text>{product.price}</Text>
 			</Box>
-
-			<Modal isOpen={isOpen} onClose={onClose}>
-				<ModalOverlay />
-
-				<ModalContent>
-					<ModalHeader>Update Product</ModalHeader>
-					<ModalCloseButton />
-					<ModalBody>
-						<VStack spacing={4}>
-							<Input
-								placeholder="Product Name"
-								name="name"
-								value={updatedProduct.name}
-								onChange={(e) =>
-									setUpdatedProduct({ ...updatedProduct, name: e.target.value })
-								}
-							/>
-							<Input
-								placeholder="Price"
-								name="price"
-								type="number"
-								value={updatedProduct.price}
-								onChange={(e) =>
-									setUpdatedProduct({
-										...updatedProduct,
-										price: e.target.value,
-									})
-								}
-							/>
-							<Input
-								placeholder="Image URL"
-								name="image"
-								value={updatedProduct.image}
-								onChange={(e) =>
-									setUpdatedProduct({
-										...updatedProduct,
-										image: e.target.value,
-									})
-								}
-							/>
-						</VStack>
-					</ModalBody>
-
-					<ModalFooter>
-						<Button
-							colorScheme="blue"
-							mr={3}
-							onClick={() => handleUpdateProduct(product._id, updatedProduct)}>
-							Update
-						</Button>
-						<Button variant="ghost" onClick={onClose}>
-							Cancel
-						</Button>
-					</ModalFooter>
-				</ModalContent>
-			</Modal>
+			<Modal></Modal>
 		</Box>
 	);
 };
