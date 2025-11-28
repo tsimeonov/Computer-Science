60145d9eb47c9ab0bb6c26770b95904ff55366df
tsimeonov <tsimeonov12@gmail.com>
2025-11-28 14:08:11 +0100

    fix the connection

--- Practice/backend/config/db.js
+++ Practice/backend/config/db.js
@@ -1,8 +1,12 @@
 import mongoose from 'mongoose';
 
-export default connectDB = async () => {
+export const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(process.env.MONGO_URI);
-		console.log(`MongoDB Connectd ${conn.connection.host}`);
-	} catch (error) {}
+		console.log(`MongoDB Connected ${conn.connection.host}`);
+	} catch (error) {
+		console.log(`Error: ${error.message}`);
+		// 1 means failure, 0 means success
+		process.exit(1);
+	}
 };
