35423845174ca0380574844ab391f39e10b98396
tsimeonov <tsimeonov12@gmail.com>
2025-11-28 14:13:10 +0100

    update

--- Practice/backend/server.mjs
+++ Practice/backend/server.mjs
@@ -8,7 +8,7 @@ const app = express();
 
 app.get('/products', (req, res) => {});
 
-console.log(process.env.MONGO_URI);
+// console.log(process.env.MONGO_URI);
 
 app.listen(5000, () => {
 	connectDB();
