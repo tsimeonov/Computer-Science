bdf9a43d24163013f36fdd50fdb19dbabbc28ad3
tsimeonov <tsimeonov12@gmail.com>
2025-11-28 15:58:48 +0100

    update file

--- Practice/backend/server.mjs
+++ Practice/backend/server.mjs
@@ -1,16 +1,38 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get('/products', (req, res) => {});
+// Witout this req.body will be undefined
+app.use(express.json());
+
+app.post('/api/products', async (req, res) => {
+	const product = req.body; // User will send this data
+
+	if (!product.name || !product.price || !product.image) {
+		return res
+			.status(400)
+			.json({ success: false, message: 'Please fill all images' });
+	}
+
+	const newProduct = new Product(product);
+
+	try {
+		await newProduct.save();
+		res.status(201).json({ success: true, data: newProduct });
+	} catch (error) {
+		console.error('Error in create product', error.message);
+		res.status(500).json({ success: false, message: 'Server Error' });
+	}
+});
 
 // console.log(process.env.MONGO_URI);
 
 app.listen(5000, () => {
 	connectDB();
-	console.log('Server atarted on port 5000');
+	console.log('Server started on port 5000');
 });
