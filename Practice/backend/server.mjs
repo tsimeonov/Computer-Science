3f6fd3d2e88f5d88bad2f92d41f45a9dbc5cc7e9
tsimeonov <tsimeonov12@gmail.com>
2025-11-29 15:20:12 +0100

    remove

--- Practice/backend/server.mjs
+++ Practice/backend/server.mjs
@@ -43,17 +43,6 @@ app.post('/api/products', async (req, res) => {
 
 // console.log(process.env.MONGO_URI);
 
-app.delete('/api/products/:id', async (req, res) => {
-	const { id } = req.params;
-
-	try {
-		await Product.findByIdAndDelete(id);
-		res.status(200).json({ success: true, message: 'Product deleted' });
-	} catch (error) {
-		res.status(404).json({ success: false, message: 'Product not found' });
-	}
-});
-
 app.listen(5000, () => {
 	connectDB();
 	console.log('Server started on port 5000');
