ba8e11e28d15db9b77a138b75958f1d84e583e25
tsimeonov <tsimeonov12@gmail.com>
2025-11-30 14:27:33 +0100

    refactor

--- Practice/backend/server.mjs
+++ Practice/backend/server.mjs
@@ -30,16 +30,16 @@ app.post('/api/products', async (req, res) => {
 	}
 });
 
-// app.delete('/api/products/:id', async (req, res) => {
-// 	const { id } = req.params;
+app.delete('/api/products/:id', async (req, res) => {
+	const { id } = req.params;
 
-// 	try {
-// 		await Product.findByIdAndDelete(id);
-// 		res.status(200).json({ success: true, message: 'Product deleted' });
-// 	} catch (error) {
-// 		res.status(404).json({ success: false, message: 'Product not found' });
-// 	}
-// });
+	try {
+		await Product.findByIdAndDelete(id);
+		res.status(200).json({ success: true, message: 'Product deleted' });
+	} catch (error) {
+		res.status(404).json({ success: false, messaage: 'Product not found' });
+	}
+});
 
 // console.log(process.env.MONGO_URI);
 
