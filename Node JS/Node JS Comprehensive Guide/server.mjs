f63952ea0ca9d93cd75a0727b0e280c111ba6754
tsimeonov <tsimeonov12@gmail.com>
2025-11-30 19:23:01 +0100

    update

--- Node JS/Node JS Comprehensive Guide/server.mjs
+++ Node JS/Node JS Comprehensive Guide/server.mjs
@@ -6,11 +6,3 @@
 // 		`Server is listening to http://localhost:${server.address().port}`
 // 	);
 // });
-
-import { createServer } from 'http';
-
-const server = createServer();
-
-server.listen(8080, () => {
-	console.log(`Server is listening on port ${server.address().port}`);
-});
