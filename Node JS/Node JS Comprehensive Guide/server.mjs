0c8583da6b0669ebfe8f780c8314a801dddfffa9
tsimeonov <tsimeonov12@gmail.com>
2025-12-03 20:32:30 +0100

    update

--- Node JS/Node JS Comprehensive Guide/server.mjs
+++ Node JS/Node JS Comprehensive Guide/server.mjs
@@ -7,14 +7,14 @@
 // 	);
 // });
 
-import { createServer } from 'http';
+// import { createServer } from 'http';
 
-const server = createServer((request, response) => {
-	response.writeHead(200, { 'content-type': 'text/plain; charset=utf-8' });
-	response.write('Hello ');
-	response.end(' World\n');
-});
+// const server = createServer((request, response) => {
+// 	response.writeHead(200, { 'content-type': 'text/plain; charset=utf-8' });
+// 	response.write('Hello ');
+// 	response.end(' World\n');
+// });
 
-server.listen(8080, () => {
-	console.log(`Server is running on ${server.address().port}`);
-});
+// server.listen(8080, () => {
+// 	console.log(`Server is running on ${server.address().port}`);
+// });
