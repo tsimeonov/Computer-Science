c1f6794e01f697ac87fd9760bd4db60c612fc4eb
tsimeonov <tsimeonov12@gmail.com>
2025-12-05 14:26:01 +0100

    write it again

--- Node JS/Node JS Comprehensive Guide/server.mjs
+++ Node JS/Node JS Comprehensive Guide/server.mjs
@@ -7,23 +7,41 @@
 // 	);
 // });
 
+// import { createServer } from 'http';
+
+// const server = createServer((request, response) => {
+// 	response.writeHead(200, { 'content-type': 'text/html; charset=utf-8' });
+
+// 	const url = new URL(request.url, 'http://localhost:8000');
+
+// 	const body = `
+//   <!DOCTYPE html>
+//   <html>
+//     <head>
+//       <meta charset="utf-8">
+//       <title>Node.js Demo</title>
+//       <body>
+//         <h1 style="color: green">${url.searchParams.get('name') || 'Guest'}</h1>
+//       </body>
+//     </head>
+//   </html>`;
+
+// 	response.end(body);
+// });
+
+// server.listen(8000, () => {
+// 	console.log(`Server is running on port ${server.address().port}`);
+// });
+
 import { createServer } from 'http';
 
 const server = createServer((request, response) => {
-	response.writeHead(200, { 'content-type': 'text/html; charset=utf=8' });
-
-	const url = new URL(request.url, 'http://localhost:8000');
+	response.writeHead(200, { 'content-type': 'text/html; charset=utf-8' });
 
 	const body = `
   <!DOCTYPE html>
   <html>
-    <head>
-      <meta charset="utf-8">
-      <title>Node.js Demo</title>
-      <body>
-        <h1 style="color: green">${url.searchParams.get('name')}</h1>
-      </body>
-    </head>
+
   </html>`;
 
 	response.end(body);
