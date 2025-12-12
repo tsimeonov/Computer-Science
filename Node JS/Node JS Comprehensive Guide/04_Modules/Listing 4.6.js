50f31d706da327e9a01e5688898a0fcb31c96662
tsimeonov <tsimeonov12@gmail.com>
2025-12-12 13:57:31 +0100

    File and directory name

---
+++ Node JS/Node JS Comprehensive Guide/04_Modules/Listing 4.6.js
@@ -0,0 +1,8 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+
+console.log(_filename);
+console.log(_dirname);
