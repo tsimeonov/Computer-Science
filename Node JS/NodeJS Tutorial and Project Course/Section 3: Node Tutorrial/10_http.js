1a1796b190ae8c8efef74cf58eee65e99ad2242c
tsimeonov <tsimeonov12@gmail.com>
2025-12-14 14:14:30 +0100

    update 10_http

--- Node JS/NodeJS Tutorial and Project Course/Section 3: Node Tutorrial/10_http.js
+++ Node JS/NodeJS Tutorial and Project Course/Section 3: Node Tutorrial/10_http.js
@@ -1,8 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-	res.write('Welcome to our home page');
-	res.end();
+	if (req.url === '/') {
+		res.end('Welcome to our home page');
+		return;
+	}
+	if (req.url === '/about') {
+		res.end('Here is our short history');
+		return;
+	}
+
+	res.end(`
+    <h1>Ooops!</h1>
+    <p>We cant find the page you are looking for</p>
+    <a href="/">Back Home</a>
+    `);
 });
 
 server.listen(5000);
