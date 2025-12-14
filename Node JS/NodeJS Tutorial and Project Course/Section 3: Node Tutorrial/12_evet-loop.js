5126c15d6e60c1b687f4269f85cbd7f33d6b8097
tsimeonov <tsimeonov12@gmail.com>
2025-12-14 16:06:28 +0100

    add file 12_event loop

---
+++ Node JS/NodeJS Tutorial and Project Course/Section 3: Node Tutorrial/12_evet-loop.js
@@ -0,0 +1,15 @@
+const { readFile } = require('fs');
+
+console.log('started first task');
+
+// CHECK FILE PATH!!!
+readFile('./content/first.txt', 'utf-8', (err, result) => {
+	if (err) {
+		console.log(err);
+		return;
+	}
+	console.log(result);
+	console.log('complete first task');
+});
+
+console.log('starting next task');
