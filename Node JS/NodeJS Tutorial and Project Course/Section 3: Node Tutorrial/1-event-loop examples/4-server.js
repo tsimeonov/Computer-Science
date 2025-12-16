27c49ad92a5454915845ae5cf311855afa8599c7
tsimeonov <tsimeonov12@gmail.com>
2025-12-16 19:48:17 +0100

    4 server

---
+++ Node JS/NodeJS Tutorial and Project Course/Section 3: Node Tutorrial/1-event-loop examples/4-server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+	console.log('request event');
+	res.end('Hello World');
+});
+
+server.listen(5000, () => {
+	console.log(`Server listening om port: 5000`);
+});
