f3d591e5b20e33f7bb50ec19cce8caecbf6e0ad7
tsimeonov <tsimeonov12@gmail.com>
2025-12-19 15:22:21 +0100

    update

--- Node JS/NodeJS Tutorial and Project Course/Section 4: Express/1-http.js
+++ Node JS/NodeJS Tutorial and Project Course/Section 4: Express/1-http.js
@@ -1,28 +1,28 @@
-// const http = require('http');
+const http = require('http');
 
-// const server = http.createServer((req, res) => {
-// 	// console.log(req.method);
-// 	// console.log(req.url);
-// 	const url = req.url;
+const server = http.createServer((req, res) => {
+	// console.log(req.method);
+	// console.log(req.url);
+	const url = req.url;
 
-// 	// Home page
-// 	if (url === '/') {
-// 		res.writeHead(200, { 'content-type': 'text/html' });
-// 		res.write('<h1>Home page</h1>');
-// 		res.end();
-// 	}
-// 	// About page
-// 	else if (url === '/about') {
-// 		res.writeHead(200, { 'content-type': 'text/html' });
-// 		res.write('<h1>About page</h1>');
-// 		res.end();
-// 	}
-// 	// Not found
-// 	else {
-// 		res.writeHead(404, { 'content-type': 'text/html' });
-// 		res.write('<h1>Page not found</h1>');
-// 		res.end();
-// 	}
-// });
+	// Home page
+	if (url === '/') {
+		res.writeHead(200, { 'content-type': 'text/html' });
+		res.write('<h1>Home page</h1>');
+		res.end();
+	}
+	// About page
+	else if (url === '/about') {
+		res.writeHead(200, { 'content-type': 'text/html' });
+		res.write('<h1>About page</h1>');
+		res.end();
+	}
+	// Not found
+	else {
+		res.writeHead(404, { 'content-type': 'text/html' });
+		res.write('<h1>Page not found</h1>');
+		res.end();
+	}
+});
 
-// server.listen(5000);
+server.listen(5000);
