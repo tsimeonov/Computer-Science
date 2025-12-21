03191e858188efafceb94263bde5d9363a819f55
tsimeonov <tsimeonov12@gmail.com>
2025-12-21 14:08:39 +0100

    again

--- Node JS/NodeJS Tutorial and Project Course/Section 4: Express/5-params-quer.js
+++ Node JS/NodeJS Tutorial and Project Course/Section 4: Express/5-params-quer.js
@@ -14,16 +14,17 @@ app.get('/api/products', (req, res) => {
 	res.json(newProducts);
 });
 
-app.get('/api/products/:productID', (req, res) => {
-	// console.log(req);
-	// console.log(req.params);
-	const { productID } = req.params;
+// app.get('/api/products/:productID', (req, res) => {
+// 	const { productID } = req.params;
 
-	const singleProduct = products.find((product) => {
-		return product.id === Number(productID);
-	});
-	res.json(singleProduct);
-});
+// 	const singleProduct = products.find((product) => {
+// 		return product.id === Number(productID);
+// 	});
+// 	if (!singleProduct) {
+// 		return res.status(404).send("Product doesn't exit");
+// 	}
+// 	res.json(singleProduct);
+// });
 
 app.listen(5000, () => {
 	console.log(`Server listening on port 5000...`);
