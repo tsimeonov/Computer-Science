e8c599ac5c60925064322cde128e7677a8296dd5
tsimeonov <tsimeonov12@gmail.com>
2025-12-24 15:02:21 +0100

    update

--- Node JS/NodeJS Tutorial and Project Course/Section 4: Express/5-params-quer.js
+++ Node JS/NodeJS Tutorial and Project Course/Section 4: Express/5-params-quer.js
@@ -31,6 +31,29 @@ app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
 	res.send('Hello world');
 });
 
+// app.get('/api/v1/query', (req, res) => {
+// 	// console.log(req.query);
+// 	const { search, limit } = req.query;
+// 	let sortedProducts = [...products];
+
+// 	if (search) {
+// 		sortedProducts = sortedProducts.filter((product) => {
+// 			return product.name.startsWith(search);
+// 		});
+// 	}
+// 	if (limit) {
+// 		sortedProducts = sortedProducts.slice(0, Number(limit));
+// 	}
+// 	if (sortedProducts.length < 1) {
+// 		// res.status(200).send('No Products matched your serach');
+// 		return res.status(200).json({ success: true, data: [] });
+// 	}
+
+// 	res.status(200).json(sortedProducts);
+// });
+
+app.get;
+
 app.listen(5000, () => {
 	console.log(`Server listening on port 5000...`);
 });
