e875e329fae2f1707a0df9bbf33a4a9046a0d8b0
tsimeonov <tsimeonov12@gmail.com>
2025-12-25 14:50:27 +0100

    7-middleware-use

--- Node JS/NodeJS Tutorial and Project Course/Section 4: Express/6-middleware-basic.js
+++ Node JS/NodeJS Tutorial and Project Course/Section 4: Express/6-middleware-basic.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const app = express();
-const logger = require('./logger');
-// req => middleware => res
 
-app.use('/api', logger);
+//  req => middleware => res
 
-app.get('/', (req, res) => {
-	res.send('Home Page');
+const logger = (req, res, next) => {
+	const method = req.method;
+	const url = req.url;
+	const time = new Date().getFullYear();
+	console.log(method, url, time);
+	next();
+};
+
+app.get('/', logger, (req, res) => {
+	res.send('Home');
 });
-
-app.get('/about', (req, res) => {
-	res.send('About Page');
-});
-
-app.get('/products', (req, res) => {
-	res.send('Products Page');
+app.get('/about', logger, (req, res) => {
+	res.send('About');
 });
 
 app.listen(5000, () => {
-	console.log(`Server listening on port 5000....`);
+	console.log('Server is listening on port 5000....');
 });
