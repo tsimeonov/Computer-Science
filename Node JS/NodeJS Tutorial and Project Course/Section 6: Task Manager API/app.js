9860d54c9b95974940ea4eed4d0b579faa06c0d5
tsimeonov <tsimeonov12@gmail.com>
2025-12-25 15:49:55 +0100

    add coments

--- Node JS/NodeJS Tutorial and Project Course/Section 6: Task Manager API/app.js
+++ Node JS/NodeJS Tutorial and Project Course/Section 6: Task Manager API/app.js
@@ -9,6 +9,12 @@ app.get('/hello', (req, res) => {
 	res.send('Task manager app');
 });
 
+// app.get("api/v1/tasks") => get all the tasks
+// app.post("api/v1/tasks") => create a new task
+// app.get("api/v1/tasks/:id") => get single tasks
+// app.patch("api/v1/tasks/:id") => update task
+// app.delete("api/v1/tasks/:id") => delete taks
+
 app.listen(port, () => {
 	console.log(`Server is listening on port ${port}....`);
 });
