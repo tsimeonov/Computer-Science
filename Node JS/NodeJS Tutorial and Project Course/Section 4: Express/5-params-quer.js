2b478c3acb72ef6c628a7aa3b011af978609d5ea
tsimeonov <tsimeonov12@gmail.com>
2025-12-25 14:05:03 +0100

    update

--- Node JS/NodeJS Tutorial and Project Course/Section 4: Express/5-params-quer.js
+++ Node JS/NodeJS Tutorial and Project Course/Section 4: Express/5-params-quer.js
@@ -32,28 +32,8 @@ app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
 	res.send('Hello world');
 });
 
-// app.get('/api/v1/query', (req, res) => {
-// 	// console.log(req.query);
-// 	const { search, limit } = req.query;
-// 	let sortedProducts = [...products];
-
-// 	if (search) {
-// 		sortedProducts = sortedProducts.filter((product) => {
-// 			return product.name.startsWith(search);
-// 		});
-// 	}
-// 	if (limit) {
-// 		sortedProducts = sortedProducts.slice(0, Number(limit));
-// 	}
-// 	if (sortedProducts.length < 1) {
-// 		// res.status(200).send('No Products matched your serach');
-// 		return res.status(200).json({ success: true, data: [] });
-// 	}
-
-// 	res.status(200).json(sortedProducts);
-// });
-
 app.get('/api/v1/query', (req, res) => {
+	// console.log(req.query);
 	const { search, limit } = req.query;
 	let sortedProducts = [...products];
 
@@ -62,14 +42,14 @@ app.get('/api/v1/query', (req, res) => {
 			return product.name.startsWith(search);
 		});
 	}
-
 	if (limit) {
 		sortedProducts = sortedProducts.slice(0, Number(limit));
 	}
-
 	if (sortedProducts.length < 1) {
+		// res.status(200).send('No Products matched your serach');
 		return res.status(200).json({ success: true, data: [] });
 	}
+
 	res.status(200).json(sortedProducts);
 });
 
