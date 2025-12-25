c38113e6147bcef96da0edb502e3f0d8ee9b0c46
tsimeonov <tsimeonov12@gmail.com>
2025-12-25 15:46:10 +0100

    app.js

--- Node JS/NodeJS Tutorial and Project Course/Section 6: Task Manager API/app.js
+++ Node JS/NodeJS Tutorial and Project Course/Section 6: Task Manager API/app.js
@@ -1,1 +1,14 @@
-console.log('Task Manager App');
+const express = require('express');
+const app = express();
+
+const port = 5000;
+
+// routes
+
+app.get('/hello', (req, res) => {
+	res.send('Task manager app');
+});
+
+app.listen(port, () => {
+	console.log(`Server is listening on port ${port}....`);
+});
