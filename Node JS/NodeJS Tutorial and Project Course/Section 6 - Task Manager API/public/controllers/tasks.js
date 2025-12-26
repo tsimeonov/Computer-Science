0bfbf0e68f1c13964ac72751fd92376ac517dc39
tsimeonov <tsimeonov12@gmail.com>
2025-12-26 13:30:10 +0100

    update routes

--- Node JS/NodeJS Tutorial and Project Course/Section 6 - Task Manager API/public/controllers/tasks.js
+++ Node JS/NodeJS Tutorial and Project Course/Section 6 - Task Manager API/public/controllers/tasks.js
@@ -1,7 +1,27 @@
 const getAllTasks = (req, res) => {
-	res.send('All Items');
+	res.send('Get all taksks');
+};
+
+const createTaks = (req, res) => {
+	res.send('Create all task');
+};
+
+const getTask = (req, res) => {
+	res.send('Get single task');
+};
+
+const updateTask = (req, res) => {
+	res.send('Update Task');
+};
+
+const deleteTaks = (req, res) => {
+	res.send('Delete task');
 };
 
 module.exports = {
 	getAllTasks,
+	createTaks,
+	getTask,
+	updateTask,
+	deleteTaks,
 };
--- Node JS/NodeJS Tutorial and Project Course/Section 6 - Task Manager API/public/routes/tasks.js
+++ Node JS/NodeJS Tutorial and Project Course/Section 6 - Task Manager API/public/routes/tasks.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllTasks } = require('../controllers/tasks');
+const {
+	getAllTasks,
+	createTask,
+	getTask,
+	updateTask,
+	deleteTaks,
+} = require('../controllers/tasks');
 
 router.route('/').get(getAllTasks);
 
