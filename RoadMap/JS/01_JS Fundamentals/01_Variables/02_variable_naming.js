1234678fb2414bb6172a5e7f6b697c1c3796e96d
tsimeonov <tsimeonov12@gmail.com>
2025-12-26 14:30:56 +0100

    add modules

---
+++ RoadMap/JS/01_JS Fundamentals/01_Variables/02_variable_naming.js
@@ -0,0 +1,18 @@
+// There are only two limitations
+// 1. The name must contain only letters, digits or the symbols $ and _
+// 2. The first character must not be a digit
+
+// Valid names
+let userName;
+let test123;
+
+// Valid names as well
+
+let $ = 1;
+let _ = 2;
+
+console.log(`The sum is = ${$ + _}`);
+
+// Invalid names
+// let 1a;
+// let my-name;
