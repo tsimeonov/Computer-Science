b5d7fc34ed7c0a005ae07256f1e97a0f74410fc7
tsimeonov <tsimeonov12@gmail.com>
2025-12-27 13:05:46 +0100

    GIVING THE RIGHT NAME

--- RoadMap/JS/01_JS Fundamentals/01_Variables/04_tasks.js
+++ RoadMap/JS/01_JS Fundamentals/01_Variables/04_tasks.js
@@ -12,3 +12,11 @@ admin = name;
 
 // 4. Show the value of admin using alert (must output "John")
 console.log(admin);
+
+// GIVING THE RIGHT NAME
+
+// 1. Create a variable with the name of our planet. How would you name such a variable?
+const ourPlanetName = 'Earth';
+
+// 2. Create a variable to store the name of a current visitor to a website. How would you name that variable?
+let currenrVisitor = 'Jogn';
