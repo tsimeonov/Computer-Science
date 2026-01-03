59c2ad59aa5c7774066b30866ec7c6b454ca19e4
tsimeonov <tsimeonov12@gmail.com>
2026-01-03 13:43:05 +0100

    call back functions

--- Javascript/The Complete JavaScript Course /Section 10 Functions/starter/03_higher-order-function.js
+++ Javascript/The Complete JavaScript Course /Section 10 Functions/starter/03_higher-order-function.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const oneWord = function (str) {
+	return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+	const [first, ...others] = str.split('');
+	return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher order function
+const transformer = function (str, fn) {
+	console.log(`Original string: ${str}`);
+	console.log(`Transformed string: ${fn(str)}`);
+
+	console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best', upperFirstWord);
+transformer('JavaScript is the best', oneWord);
