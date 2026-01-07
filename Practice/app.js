756c6968eed7369b4020ebe713d4357cea513458
tsimeonov <tsimeonov12@gmail.com>
2026-01-07 19:10:46 +0100

    remove

--- Practice/app.js
+++ Practice/app.js
@@ -2,15 +2,3 @@ const progress = document.getElementById('progress');
 const next = document.getElementById('next');
 const prev = document.getElementById('prev');
 const circles = document.querySelectorAll('.circle');
-
-let currentActive = 1;
-
-next.addEventListener('click', () => {
-	currentActive++;
-
-	if (currentActive > circles.length) {
-		currentActive = circles.length;
-	}
-
-	update();
-});
