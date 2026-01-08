5ac16ced6da762f622b207c547dab4aeaeac5cfb
tsimeonov <tsimeonov12@gmail.com>
2026-01-08 19:57:22 +0100

    add

--- Practice/app.js
+++ Practice/app.js
@@ -2,3 +2,5 @@ const progress = document.getElementById('progress');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 const cirlces = document.querySelectorAll('.circle');
+
+let currentActive = 1;
