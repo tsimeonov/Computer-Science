65c5cbdc8f816a21092d5f97eead4502a4581b88
tsimeonov <tsimeonov12@gmail.com>
2026-01-14 20:10:43 +0100

    add function

--- Practice/app.js
+++ Practice/app.js
@@ -2,3 +2,28 @@ const progress = document.getElementById('progress');
 const next = document.getElementById('next');
 const prev = document.getElementById('prev');
 const cirlces = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+function update() {
+	cirlces.forEach((circle, index) => {
+		if (index < currentActive) {
+			circle.classList.add('active');
+		} else {
+			circle.classList.remove('active');
+		}
+	});
+
+	const actives = document.querySelectorAll('.active');
+
+	progress.style.width = (cirlces.length / actives.length) * 100 + '%';
+
+	if (currentActive === 1) {
+		prev.disabled = false;
+	} else if (currentActive === cirlces.length) {
+		next.disabled = false;
+	} else {
+		prev.disabled = true;
+		next.disabled = true;
+	}
+}
