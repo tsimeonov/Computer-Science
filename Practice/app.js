a58842e80d7ed432542b9b14f3f2b6b38d14e5b4
tsimeonov <tsimeonov12@gmail.com>
2026-01-14 20:06:16 +0100

    add variables

---
+++ Practice/app.js
@@ -0,0 +1,4 @@
+const progress = document.getElementById('progress');
+const next = document.getElementById('next');
+const prev = document.getElementById('prev');
+const cirlces = document.querySelectorAll('.circle');
