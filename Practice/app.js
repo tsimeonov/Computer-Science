519333dcb6afbb83ee01b368bbaf24d72e262eaf
tsimeonov <tsimeonov12@gmail.com>
2026-01-15 20:50:30 +0100

    remove

--- Practice/app.js
+++
@@ -1,4 +0,0 @@
-const progress = document.getElementById('progress');
-const next = document.getElementById('next');
-const prev = document.getElementById('prev');
-const circles = document.querySelectorAll('circle');
