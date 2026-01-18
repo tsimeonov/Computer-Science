20b19e86ffd3ff180f11fc0699d0e76491889c4b
tsimeonov <tsimeonov12@gmail.com>
2026-01-18 10:28:32 +0100

    add js

---
+++ Practice/app.js
@@ -0,0 +1,50 @@
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const cirlces = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+next.addEventListener('click', () => {
+	currentActive++;
+
+	if (currentActive > cirlces.length) {
+		currentActive = cirlces.length;
+	}
+
+	update();
+});
+
+prev.addEventListener('click', () => {
+	currentActive--;
+
+	if (currentActive < 1) {
+		currentActive = 1;
+	}
+
+	update();
+});
+
+function update() {
+	cirlces.forEach((cirlce, index) => {
+		if (index < currentActive) {
+			cirlce.classList.add('active');
+		} else {
+			cirlce.classList.remove('active');
+		}
+	});
+
+	const actives = document.querySelectorAll('.active');
+
+	progress.style.width =
+		((actives.length - 1) / (cirlces.length - 1)) * 100 + '%';
+
+	if (currentActive === 1) {
+		prev.disabled = true;
+	} else if (currentActive === cirlces.length) {
+		next.disabled = true;
+	} else {
+		prev.disabled = false;
+		next.disabled = false;
+	}
+}
