ad32aa0b5a1bbcb79e9c24c4f1e2b103024f55a1
tsimeonov <tsimeonov12@gmail.com>
2026-01-25 13:11:47 +0100

    add js

---
+++ JS Projects/06_Scroll Animation/app.js
@@ -0,0 +1,19 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBOxes);
+
+checkBOxes();
+
+function checkBOxes() {
+	const triggerBottom = (window.innerHeight / 5) * 4;
+
+	boxes.forEach((box) => {
+		const boxTop = box.getBoundingClientRect().top;
+
+		if (boxTop < triggerBottom) {
+			box.classList.add('show');
+		} else {
+			box.classList.remove('show');
+		}
+	});
+}
