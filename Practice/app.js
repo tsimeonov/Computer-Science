dfbae92e02abae55d145977e2a5efd52fa859915
tsimeonov <tsimeonov12@gmail.com>
2026-01-28 20:17:36 +0100

    update

--- Practice/app.js
+++ Practice/app.js
@@ -1,2 +1,12 @@
 const loadText = document.querySelector('.loading-text');
 const bg = document.querySelector('.bg');
+
+let load = 0;
+
+let int = setInterval(blurring, 30);
+
+function blurring() {}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+	return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
