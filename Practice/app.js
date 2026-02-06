4201d960e9bafafcffaddbb088441e23aa1cdbfb
tsimeonov <tsimeonov12@gmail.com>
2026-02-06 08:25:43 +0100

    add js

---
+++ Practice/app.js
@@ -0,0 +1,19 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBox);
+
+checkBox();
+
+function checkBox() {
+	const triggerBottom = (window.innerWidth / 4) * 4;
+
+	boxes.forEach((box) => {
+		const boxTop = box.getBoundingClientRect().top;
+
+		if (boxTop < triggerBottom) {
+			box.classList.add('show');
+		} else {
+			box.classList.remove('show');
+		}
+	});
+}
