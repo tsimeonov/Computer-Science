7d4ea08a391dcce50835a167b9ca0b0fbc41c22c
tsimeonov <tsimeonov12@gmail.com>
2026-02-14 14:20:07 +0100

    update

--- JS Projects/09_DOM Array Methods/app.js
+++ JS Projects/09_DOM Array Methods/app.js
@@ -13,7 +13,7 @@ getRandomUser();
 
 // fetch random user and add money
 async function getRandomUser() {
-	const res = await fetch('https://corsproxy.io/?https://randomuser.me/api');
+	const res = await fetch('https://randomuser.me/api');
 	const data = await res.json();
 
 	// console.log(data);
