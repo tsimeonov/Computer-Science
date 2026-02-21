8faa56d0aea67f9ff7d67757a85529b6039513e0
tsimeonov <tsimeonov12@gmail.com>
2026-02-21 15:43:46 +0100

    style

--- JS Projects/15_Theme Clock/app.js
+++ JS Projects/15_Theme Clock/app.js
@@ -46,7 +46,7 @@ function setTime() {
 	const day = time.getDay();
 	const date = time.getDate();
 	const hours = time.getHours();
-	const hoursForClock = hours >= (13 ? hours % 12 : hours);
+	const hoursForClock = hours >= 13 ? hours % 12 : hours;
 	const minutes = time.getMinutes();
 	const seconds = time.getSeconds();
 	const ampm = hours >= 12 ? 'PM' : 'AM';
