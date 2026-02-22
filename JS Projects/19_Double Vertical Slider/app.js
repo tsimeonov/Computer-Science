c0b50e2ed2945aefecaae6d69787b08b8f0e7a17
tsimeonov <tsimeonov12@gmail.com>
2026-02-22 15:02:09 +0100

    add js

--- JS Projects/19_Double Vertical Slider/app.js
+++ JS Projects/19_Double Vertical Slider/app.js
@@ -4,3 +4,26 @@ const slideLeft = document.querySelector('.left-slide');
 const upButton = document.querySelector('.up-button');
 const downButton = document.querySelector('.down-button');
 const slidesLenght = slideRight.querySelectorAll('div').length;
+
+let activeSlideIndex = 0;
+
+slideLeft.style.top = `-${(slidesLenght - 1) * 100}vh`;
+
+upButton.addEventListener('click', () => changeSlide('up'));
+downButton.addEventListener('click', () => changeSlide('down'));
+
+const changeSlide = (direction) => {
+	const sliderHeight = sliderContainer.clientHeight;
+
+	if (direction === 'up') {
+		activeSlideIndex++;
+		if (activeSlideIndex > slidesLenght - 1) {
+			activeSlideIndex = 0;
+		} else if (direction === 'down') {
+			activeSlideIndex--;
+			if (activeSlideIndex < 0) {
+				activeSlideIndex = slidesLenght - 1;
+			}
+		}
+	}
+};
