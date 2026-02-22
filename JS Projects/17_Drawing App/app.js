1e02d0eb9cb0de48a2c341d58eafc9058384d4a6
tsimeonov <tsimeonov12@gmail.com>
2026-02-22 13:16:37 +0100

    add functions

--- JS Projects/17_Drawing App/app.js
+++ JS Projects/17_Drawing App/app.js
@@ -13,3 +13,23 @@ colorEl.value = 'black';
 let color = colorEl.value;
 let x;
 let y;
+
+function drawCircle(x, y) {
+	ctx.beginPath();
+	ctx.arc(x, y, size, 0, Math.PI * 2);
+	ctx.fillStyle = color;
+	ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.strokeStyle = color;
+	ctx.lineWidth = size * 2;
+	ctx.stroke();
+}
+
+function updateSizeOnScreen() {
+	sizeEl.innerText = size;
+}
