25134a75dc460fa4aed18eae4454f63d7668a238
tsimeonov <tsimeonov12@gmail.com>
2026-02-22 10:51:52 +0100

    add variables

---
+++ JS Projects/17_Drawing App/app.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEl = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
