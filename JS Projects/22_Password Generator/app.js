dcb545eafb6f9c4ac38fefa843d922e91ae5d2ab
tsimeonov <tsimeonov12@gmail.com>
2026-02-28 14:01:56 +0100

    update

--- JS Projects/22_Password Generator/app.js
+++ JS Projects/22_Password Generator/app.js
@@ -30,24 +30,28 @@ generateEl.addEventListener('click', () => {
 	const hasUpper = uppercaseEl.checked;
 	const hasNumber = numbersEl.checked;
 	const hasSymbol = symbolsEl.checked;
+
+	resultEl.innerText = generatePassword(
+		hasLower,
+		hasUpper,
+		hasNumber,
+		hasSymbol,
+		length
+	);
 });
 
 function generatePassword(lower, upper, number, symbol, length) {
 	let generatePassword = '';
 	const typesCount = lower + upper + number + symbol;
-	const typeArr = [
-		{ lower },
-		{ upper },
-		{ number },
-		{ symbol }.filter((item) => Object.values(item[0])),
-	];
-
+	const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+		(item) => Object.values(item)[0]
+	);
 	if (typesCount === 0) {
 		return '';
 	}
 
 	for (let i = 0; i < length; i += typesCount) {
-		typeArr.forEach((type) => {
+		typesArr.forEach((type) => {
 			const funcName = Object.keys(type)[0];
 			generatePassword += randomFunc[funcName]();
 		});
