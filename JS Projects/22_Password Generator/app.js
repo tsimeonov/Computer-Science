23e0015fafa2f8d4cc2efef04cacd182cd312b09
tsimeonov <tsimeonov12@gmail.com>
2026-02-28 13:14:10 +0100

    add generatePassword()

--- JS Projects/22_Password Generator/app.js
+++ JS Projects/22_Password Generator/app.js
@@ -6,3 +6,36 @@ const numberstEl = document.getElementById('numbers');
 const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
 const clipboardEl = document.getElementById('clipboard');
+
+const randomFunc = {
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	number: getRandomNumber,
+	symbol: getRandomSymbol,
+};
+
+function generatePassword(lower, upper, number, symbol, length) {
+	let generatePassword = '';
+	const typesCount = lower + upper + number + symbol;
+	const typeArr = [
+		{ lower },
+		{ upper },
+		{ number },
+		{ symbol }.filter((item) => Object.values(item[0])),
+	];
+
+	if (typesCount === 0) {
+		return '';
+	}
+
+	for (let i = 0; i < length; i += typesCount) {
+		typeArr.forEach((type) => {
+			const funcName = Object.keys(type)[0];
+			generatePassword += randomFunc[funcName]();
+		});
+	}
+
+	const finalPassword = generatePassword.slice(0, length);
+
+	return finalPassword;
+}
