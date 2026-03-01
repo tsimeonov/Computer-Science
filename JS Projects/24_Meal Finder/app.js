5eccf5831fa3608fda1a666aaf4ab352fde14f30
tsimeonov <tsimeonov12@gmail.com>
2026-03-01 14:52:47 +0100

    add js

--- JS Projects/24_Meal Finder/app.js
+++ JS Projects/24_Meal Finder/app.js
@@ -104,3 +104,22 @@ function addMealToDOM(meal) {
     </div>
 	`;
 }
+
+// Event listeners
+submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
+
+mealsEl.addEventListener('click', (e) => {
+	const mealInfo = e.composedPath().find((item) => {
+		if (item.classList) {
+			return item.classList.contains('meal-info');
+		} else {
+			return false;
+		}
+	});
+
+	if (mealInfo) {
+		const mealID = mealInfo.getAttribute('data-mealid');
+		getMealById(mealID);
+	}
+});
