3ea070b85f200296c19ab6ebe681e6904e5cb907
tsimeonov <tsimeonov12@gmail.com>
2026-03-01 14:49:22 +0100

    addMealToDOM()

--- JS Projects/24_Meal Finder/app.js
+++ JS Projects/24_Meal Finder/app.js
@@ -71,3 +71,36 @@ function getRandomMeal() {
 			addMealToDOM(meal);
 		});
 }
+
+// Add meal to DOM
+function addMealToDOM(meal) {
+	const ingredients = [];
+
+	for (let i = 1; i <= 20; i++) {
+		if (meal[`strIngredinet${i}`]) {
+			ingrediensts.push(
+				`${meal[`strIngredient${i}`]} - ${meal['strMeasure${i}']}`
+			);
+		} else {
+			break;
+		}
+	}
+
+	single_mealEl.innerHTML = `
+	<div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map((ing) => `<li>${ing}</li>`).join('')}
+        </ul>
+      </div>
+    </div>
+	`;
+}
