252378ad902626f595560af5183f1f94ce911be0
tsimeonov <tsimeonov12@gmail.com>
2026-03-04 20:18:54 +0100

    add closeNavbar()

--- JS Projects/25_Menu Slider & Modal/app.js
+++ JS Projects/25_Menu Slider & Modal/app.js
@@ -3,3 +3,20 @@ const close = document.getElementById('close');
 const open = document.getElementById('open');
 const modal = document.getElementById('modal');
 const navbar = document.getElementById('navbar');
+
+// This function closes the navbar if user clicks anywhere outside of navbar once it's opened
+
+function closeNavbar(e) {
+	if (
+		document.body.classList.contains('show-nav') &&
+		e.target !== toggle &&
+		!toggle.contains(e.target) &&
+		e.target !== navbar &&
+		!navbar.contains(e.target)
+	) {
+		document.body.classList.toggle('show-nav');
+		document.body.addEventListener('click', closeNavbar);
+	} else if (!document.body.classList.contains('show-nav')) {
+		document.body.removeEventListener('click', closeNavbar);
+	}
+}
