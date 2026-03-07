c11c74317918d86966f252f9213007039ab85459
tsimeonov <tsimeonov12@gmail.com>
2026-03-07 14:58:41 +0100

    add showPosts()

---
+++ JS Projects/27_Infinite Scroll/app.js
@@ -0,0 +1,36 @@
+const postContainer = document.getElementById('post-container');
+const loading = document.querySelector('.loader');
+const filter = document.getElementById('filter');
+
+let limit = 5;
+let page = 1;
+
+// Fetch posts from API
+async function getPosts() {
+	const res = await fetch(
+		`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+	);
+
+	const data = await res.json();
+
+	return data;
+}
+
+// Show posts on DOM
+async function showPosts() {
+	const posts = await getPosts();
+
+	posts.foreEach((post) => {
+		const postEl = document.createElement('div');
+		postEl.classList.add('post');
+		postEl.innerHTML = `
+			<div class="number">${post.id}</div>
+			<div class="post-info">
+				<h2 class="post-title">${post.title}</h2>
+				<p class="post-body">${post.body}</p>
+			</div>
+		`;
+
+		postContainer.appendChild(postEl);
+	});
+}
