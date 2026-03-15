8d87b2206889ff1dd2e23937593b5fa5819988ee
tsimeonov <tsimeonov12@gmail.com>
2026-03-15 14:55:28 +0100

    style

--- JS Projects/58_Product list with cart/product-list-with-cart-main/app.js
+++ JS Projects/58_Product list with cart/product-list-with-cart-main/app.js
@@ -1,24 +1,44 @@
-const AllDessertImg = document.querySelectorAll('.dessert-img');
+const container = document.querySelector('.desserts-container');
 
-function updateImage() {
-	const viewportWidth = window.innerWidth;
+// 1. Create the function to fetch the data
+async function loadAndRender() {
+	const response = await fetch('./data.json');
+	const dessertData = await response.json();
 
-	AllDessertImg.forEach((img) => {
-		const name = img.getAttribute('src');
+	// 2. Define render logic
+	function renderDesserts() {
+		const viewportWidth = window.innerWidth;
 
-		// Update desktop image
-		if (viewportWidth < 375) {
-			img.src = `./assets/images/image-${name}-mobile.jpg`;
-		} else if (viewportWidth < 768) {
-			img.src = `./assets/images/image-${name}-tablet.jpg`;
-		} else {
-			img.src = `./assets/images/image-${name}-desktop.jpg`;
-		}
-	});
+		// Create HTML string
+
+		const html = dessertData
+			.map((dessert) => {
+				// Determine the correct image path based on width
+				let imageSrc = dessert.image.desktop;
+
+				// Update desktop image
+				if (viewportWidth < 375) {
+					imageSrc = dessert.image.mobile;
+				} else if (viewportWidth < 768) {
+					imageSrc = dessert.image.tablet;
+				}
+
+				return `
+      <div class="dessert-card">
+        <img class="dessert-img" src="${imageSrc}" alt="${dessert.name}">
+        <div class="dessert-button">Add to Cart</div>
+        <div class="dessert-title">${dessert.category}</div>
+        <div class="dessert-body">${dessert.name}</div>
+        <div class="dessert-price">$${dessert.price.toFixed(2)}</div>
+      </div>
+    `;
+			})
+			.join('');
+		container.innerHTML = html;
+	}
+	renderDesserts();
+
+	window.addEventListener('resize', renderDesserts);
 }
 
-updateImage();
-
-window.addEventListener('resize', updateImage);
-
-// console.log(dessertImg);
+loadAndRender();
