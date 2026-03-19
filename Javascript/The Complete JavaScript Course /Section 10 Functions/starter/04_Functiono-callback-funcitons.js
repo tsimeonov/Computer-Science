e3076fe6826480dff8d44de3dc6f2366e4d37ccc
tsimeonov <tsimeonov12@gmail.com>
2026-03-19 14:04:31 +0100

    update

--- Javascript/The Complete JavaScript Course /Section 10 Functions/starter/04_Functiono-callback-funcitons.js
+++ Javascript/The Complete JavaScript Course /Section 10 Functions/starter/04_Functiono-callback-funcitons.js
@@ -24,15 +24,3 @@
 // };
 
 // document.body.addEventListener('click', high5);
-
-const oneWord = function (str) {
-	return str.replace(/ /g, '').toLowerCase();
-};
-
-const transformer = function (str, fn) {
-	console.log(`Original string: ${str}`);
-	console.log(`Transformed string: ${fn(str)}`);
-	console.log(`Transformed by: ${fn.name}`);
-};
-
-transformer(`JavaScript is the best`, oneWord);
