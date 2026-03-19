620109a702f7591387f1ca376aacb8ec549a6954
tsimeonov <tsimeonov12@gmail.com>
2026-03-19 15:55:16 +0100

    update

--- Javascript/The Complete JavaScript Course /Section 10 Functions/starter/06_Call and Apply.js
+++ Javascript/The Complete JavaScript Course /Section 10 Functions/starter/06_Call and Apply.js
@@ -1,13 +1,38 @@
 const lufthanse = {
-	airline: 'Lufthansa',
+	airline: 'Lufhthansa',
 	iataCode: 'LH',
 	bookings: [],
-	book(flighNum, name) {
+	book(flightNum, name) {
 		console.log(
-			`${name} booked a set on ${this.airline} flight ${this.iataCode}${flighNum}`
+			`${name} booked a sit on ${this.airline} flight ${this.iataCode}${flightNum}`
 		);
+		this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
 	},
 };
 
 lufthanse.book(239, 'Todor Simeonov');
 lufthanse.book(643, 'John Smith');
+console.log(lufthanse);
+
+const eurowings = {
+	airline: 'Eurowings',
+	iataCode: 'EW',
+	bookings: [],
+};
+
+const book = lufthanse.book;
+
+// Does NOT Work
+// book(23, 'Sarah Williams');
+
+// Call method
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthanse, 411, 'Mary Cooper');
+console.log(lufthanse);
+
+// Apply method
+const flightData = [503, 'George Cooper'];
+book.apply(lufthanse, flightData);
+console.log(flightData);
