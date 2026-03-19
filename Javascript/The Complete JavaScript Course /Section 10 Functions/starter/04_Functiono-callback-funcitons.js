ff461a85b442de548f36d41a6005439575fe0c21
tsimeonov <tsimeonov12@gmail.com>
2026-03-19 14:09:25 +0100

    add

--- Javascript/The Complete JavaScript Course /Section 10 Functions/starter/04_Functiono-callback-funcitons.js
+++ Javascript/The Complete JavaScript Course /Section 10 Functions/starter/04_Functiono-callback-funcitons.js
@@ -24,3 +24,22 @@
 // };
 
 // document.body.addEventListener('click', high5);
+
+const oneWord = function (str) {
+	return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+	const [first, ...others] = str.split(' ');
+
+	return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+	console.log(`Original string: ${str}`);
+	console.log(`Transformed string: ${fn(str)}`);
+	console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best', oneWord);
+transformer('JavaScript is the best', upperFirstWord);
