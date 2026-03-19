295e98aae71d806fe6f3d465c30ffdccac8ce7a1
tsimeonov <tsimeonov12@gmail.com>
2026-03-19 13:47:19 +0100

    update

--- Javascript/The Complete JavaScript Course /Section 10 Functions/starter/04_Functiono-callback-funcitons.js
+++ Javascript/The Complete JavaScript Course /Section 10 Functions/starter/04_Functiono-callback-funcitons.js
@@ -1,26 +1,38 @@
+// const oneWord = function (str) {
+// 	return str.replace(/ /g, '').toLowerCase();
+// };
+
+// const upperFirstWord = function (str) {
+// 	const [first, ...others] = str.split(' ');
+
+// 	return [first.toUpperCase(), ...others].join(' ');
+// };
+
+// // Higher order function
+// const transformer = function (str, fn) {
+// 	console.log(`Original string ${str}`);
+// 	console.log(`Transformed string: ${fn(str)}`);
+
+// 	console.log(`Transformed by: ${fn.name}`);
+// };
+
+// transformer('JavaScripts is the best', upperFirstWord);
+// transformer('JavaScripts is the best', oneWord);
+
+// const high5 = function () {
+// 	console.log('👋');
+// };
+
+// document.body.addEventListener('click', high5);
+
 const oneWord = function (str) {
 	return str.replace(/ /g, '').toLowerCase();
 };
 
-const upperFirstWord = function (str) {
-	const [first, ...others] = str.split(' ');
-
-	return [first.toUpperCase(), ...others].join(' ');
-};
-
-// Higher order function
 const transformer = function (str, fn) {
-	console.log(`Original string ${str}`);
+	console.log(`Original string: ${str}`);
 	console.log(`Transformed string: ${fn(str)}`);
-
 	console.log(`Transformed by: ${fn.name}`);
 };
 
-transformer('JavaScripts is the best', upperFirstWord);
-transformer('JavaScripts is the best', oneWord);
-
-const high5 = function () {
-	console.log('👋');
-};
-
-document.body.addEventListener('click', high5);
+transformer(`JavaScript is the best`, oneWord);
