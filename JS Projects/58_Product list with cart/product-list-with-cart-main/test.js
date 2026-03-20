3b0e072f3ad0a4f36c6e905ca3be58fd5366ff1e
tsimeonov <tsimeonov12@gmail.com>
2026-03-20 11:46:20 +0100

    update

--- JS Projects/58_Product list with cart/product-list-with-cart-main/test.js
+++ JS Projects/58_Product list with cart/product-list-with-cart-main/test.js
@@ -10,17 +10,35 @@ async function loadAndRender() {
 	function renderDesserts() {
 		const viewportWidth = window.innerWidth;
 
-		const html = dessertData.map((dessert) => {
-			// Determine the correct image path based on width
-			let imageSrc = dessert.image.desktop;
+		const html = dessertData
+			.map((dessert) => {
+				// Determine the correct image path based on width
+				let imageSrc = dessert.image.desktop;
 
-			if (viewportWidth < 375) {
-				imageSrc = dessert.image.mobile;
-			} else if (viewportWidth < 768) {
-				imageSrc = dessert.image.tablet;
-			}
-		});
+				if (viewportWidth < 375) {
+					imageSrc = dessert.image.mobile;
+				} else if (viewportWidth < 768) {
+					imageSrc = dessert.image.tablet;
+				}
+
+				return `
+      <div>
+        <img class="desset-img" src="${imageSrc}" alt="${dessert.name}"/>
+        <div class="dessert-button">
+          <img src="./assets/images/icon-add-to-cart.svg"/>
+          Add to cart
+        </div>
+      </div>
+      `;
+			})
+			.join('');
+
+		container.innerHTML = html;
 	}
+
+	renderDesserts();
+
+	window.addEventListener('resize', renderDesserts);
 }
 
 loadAndRender();
