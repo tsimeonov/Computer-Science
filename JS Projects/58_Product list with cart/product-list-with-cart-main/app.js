2733b13ebecaed8051827c05e0f3e99e5273d93d
tsimeonov <tsimeonov12@gmail.com>
2026-03-25 19:40:44 +0100

    update

--- JS Projects/58_Product list with cart/product-list-with-cart-main/app.js
+++ JS Projects/58_Product list with cart/product-list-with-cart-main/app.js
@@ -1,6 +1,10 @@
 const container = document.querySelector('.desserts-container');
 
-const dessertCard = document.querySelector('.dessert-card');
+const originalCard = document.querySelector('.dessert-card');
+
+// Clear the container of the manual HTML placeholder
+const dessertCard = originalCard.cloneNode(true);
+container.innerHTML = '';
 
 // 1. Create the function to fetch the data
 async function loadAndRender() {
@@ -8,9 +12,6 @@ async function loadAndRender() {
 	const dessertData = await response.json();
 
 	console.log(dessertData);
-
-	// Clear the container of the manual HTML placeholder
-	container.innerHTML = '';
 
 	// 2. Define render logic
 	function renderDesserts() {
@@ -21,28 +22,33 @@ async function loadAndRender() {
 
 		const width = window.innerWidth;
 
-		// 2. Clone the tempalte we grabbed earlier
-		const newCard = dessertCard.cloneNode(true);
+		dessertData.forEach((dessert) => {
+			// 2. Clone the tempalte we grabbed earlier
+			const newCard = dessertCard.cloneNode(true);
 
-		// 3. Update Image based on Screen Width
-		const img = newCard.querySelector('.dessert-img');
-		let imageSrc = dessert.image.mobile;
+			// 3. Update Image based on Screen Width
+			const img = newCard.querySelector('.dessert-img');
+			let imageSrc = dessert.image.mobile;
 
-		if (width >= 1024) imageSrc = dessert.image.desktop;
-		else if (width >= 768) imageSrc = dessert.image.tablet;
+			if (width >= 1024) imageSrc = dessert.image.desktop;
+			else if (width >= 768) imageSrc = dessert.image.tablet;
 
-		img.src = imageSrc;
-		img.alt = dessert.name;
+			img.src = imageSrc;
+			img.alt = dessert.name;
 
-		// 4, Fill in the Text data
-		newCard.querySelector('.dessert-title').textContent = dessert.category;
-		newCard.querySelector('.dessert-body').textContent = dessert.name;
-		newCard.querySelector('.dessert-price').textContent =
-			dessert.price.toFixed(2);
+			// 4, Fill in the Text data
+			newCard.querySelector('.dessert-title').textContent = dessert.category;
+			newCard.querySelector('.dessert-body').textContent = dessert.name;
+			newCard.querySelector('.dessert-price').textContent =
+				dessert.price.toFixed(2);
 
-		// 5. Put it back i nthe container
-		container.appendChild(newCard);
+			// 5. Put it back i nthe container
+			container.appendChild(newCard);
+		});
 	}
+
+	// Inital call to show the items
+	renderDesserts();
 
 	window.addEventListener('resize', renderDesserts);
 }
