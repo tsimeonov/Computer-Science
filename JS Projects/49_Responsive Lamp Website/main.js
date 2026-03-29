028159df2970b9b879aeb22d747d95b9113c42f5
tsimeonov <tsimeonov12@gmail.com>
2026-03-29 17:31:27 +0200

    add html

--- JS Projects/49_Responsive Lamp Website/main.js
+++ JS Projects/49_Responsive Lamp Website/main.js
@@ -22,6 +22,16 @@ if (navClose) {
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
+const navLink = document.querySelectorAll('.nav-link');
+
+const linkAction = () => {
+	const navMenu = document.getElementById('nav-menu');
+	// When we click on each nav-link, we remove the show-menu
+	navMenu.classList.remove('show-menu');
+};
+
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 /*=============== SWIPER POPULAR ===============*/
