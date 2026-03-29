af56e5106719ea14df6853f4380c110088ef8e54
tsimeonov <tsimeonov12@gmail.com>
2026-03-29 17:02:24 +0200

    add js

--- JS Projects/49_Responsive Lamp Website/main.js
+++ JS Projects/49_Responsive Lamp Website/main.js
@@ -1,5 +1,25 @@
 /*=============== SHOW MENU ===============*/
 
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+// MENU SHOW
+// Valiadate if constant exists
+if (navToggle) {
+	navToggle.addEventListener('click', () => {
+		navMenu.classList.add('show-menu');
+	});
+}
+
+// MENU HIDDEN
+// Validate if constant exists
+if (navClose) {
+	navClose.addEventListener('click', () => {
+		navMenu.classList.remove('show-menu');
+	});
+}
+
 /*=============== REMOVE MENU MOBILE ===============*/
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
