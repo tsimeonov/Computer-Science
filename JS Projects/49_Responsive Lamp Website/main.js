8806db68b6ad7a98f3783de0197f84e13cbadc84
tsimeonov <tsimeonov12@gmail.com>
2026-04-02 19:59:18 +0200

    add js

--- JS Projects/49_Responsive Lamp Website/main.js
+++ JS Projects/49_Responsive Lamp Website/main.js
@@ -33,6 +33,14 @@ const linkAction = () => {
 navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+	const header = document.getElementById('header');
+	// When the scroll is greater than 50 viewport height, and the scroll-header calls
+	this.srollY >= 50
+		? header.classList.add('bg-header')
+		: header.classList.remove('bg-header');
+};
+window.addEventListener('scroll', scrollHeader);
 
 /*=============== SWIPER POPULAR ===============*/
 
