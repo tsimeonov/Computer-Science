1ae24303fe19e714fa7734732aaf0c415397dd60
tsimeonov <tsimeonov12@gmail.com>
2026-04-06 13:59:07 +0200

    remove menu mobile

--- JS Projects/49_Responsive Lamp Website/main.js
+++ JS Projects/49_Responsive Lamp Website/main.js
@@ -20,6 +20,15 @@ if (navClose) {
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav-link');
+
+const linkAction = () => {
+	const navMenu = document.getElementById('nav-menu');
+	// When we click on each nav-link, we remove the show-menu class
+	navMenu.classList.remove('show-menu');
+};
+
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
