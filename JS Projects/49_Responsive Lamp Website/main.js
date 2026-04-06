ba6d66a2c659687127c3bc436c7e599f74a4a191
tsimeonov <tsimeonov12@gmail.com>
2026-04-06 13:56:43 +0200

    update code

---
+++ JS Projects/49_Responsive Lamp Website/main.js
@@ -0,0 +1,36 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+// MENU SHOW
+// Validate if constant exists
+if (navToggle) {
+	navToggle.addEventListener('click', () => {
+		navMenu.classList.add('show-menu');
+	});
+}
+
+// MENU HIDDEN
+// Validate if constant exists
+if (navClose) {
+	navClose.addEventListener('click', () => {
+		navMenu.classList.remove('show-menu');
+	});
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+/*=============== SWIPER POPULAR ===============*/
+
+/*=============== CHOOSE FAQ ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== DARK LIGHT THEME ===============*/
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
