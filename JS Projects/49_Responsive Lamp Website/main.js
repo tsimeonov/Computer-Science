f158ad9bc7464db08b6dff1899a30a93c9d7337f
tsimeonov <tsimeonov12@gmail.com>
2026-04-11 15:14:39 +0200

    update

--- JS Projects/49_Responsive Lamp Website/main.js
+++ JS Projects/49_Responsive Lamp Website/main.js
@@ -109,6 +109,28 @@ const scrollUp = () => {
 window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+	const scrollY = window.pageYOffset;
+
+	sections.forEach((current) => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 58;
+		const sectionId = current.getAttribute('id');
+		const sectionsClass = document.querySelector(
+			'.nav-menu a[href*=' + sectionId + ']'
+		);
+
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			sectionsClass.classList.add('active-link');
+		} else {
+			sectionsClass.classList.remove('active-link');
+		}
+	});
+};
+
+window.addEventListener('scroll', scrollActive);
 
 /*=============== DARK LIGHT THEME ===============*/
 
