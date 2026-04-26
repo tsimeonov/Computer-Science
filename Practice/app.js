e24222f8f0717009b5a9a8af28a74314eeeded5a
tsimeonov <tsimeonov12@gmail.com>
2026-04-26 14:50:57 +0200

    clear

--- Practice/app.js
+++ Practice/app.js
@@ -12,18 +12,3 @@ console.log(car);
 console.log('-----------');
 
 // Factory function
-
-class Car {
-	constructor(make, model) {
-		this.make = make;
-		this.model = model;
-		this.start();
-	}
-
-	start() {
-		console.log('Brrrrrr');
-	}
-}
-
-const car2 = new Car('Toyota', 'Rav5');
-console.log(car2);
