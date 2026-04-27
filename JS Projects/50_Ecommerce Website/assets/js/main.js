d1237fc939b96f99b0d414a0f32a92ae56a2f7d8
tsimeonov <tsimeonov12@gmail.com>
2026-04-27 18:19:27 +0200

    add files

---
+++ JS Projects/50_Ecommerce Website/assets/js/main.js
@@ -0,0 +1,11 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+/*==================== NAVBAR SELECT ====================*/
+
+/*==================== SHOW SCROLL UP ====================*/
