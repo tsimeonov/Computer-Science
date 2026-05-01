3b561fc5e3ec67d05de8ecb3f505d352895e9be6
tsimeonov <tsimeonov12@gmail.com>
2026-05-01 19:44:45 +0200

    update

--- JS Projects/50_Ecommerce Website/assets/js/main.js
+++ JS Projects/50_Ecommerce Website/assets/js/main.js
@@ -7,5 +7,8 @@
 /* Validate if constant exists */
 
 /*==================== NAVBAR SELECT ====================*/
+NiceSelect.bind(document.getElementById('navbar-select'), {
+	searchable: false,
+});
 
 /*==================== SHOW SCROLL UP ====================*/
