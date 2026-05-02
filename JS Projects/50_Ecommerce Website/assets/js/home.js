e1d0f3a6dca8e034c2b34a35697e239a005c8158
tsimeonov <tsimeonov12@gmail.com>
2026-05-02 13:42:23 +0200

    update

--- JS Projects/50_Ecommerce Website/assets/js/home.js
+++ JS Projects/50_Ecommerce Website/assets/js/home.js
@@ -1,4 +1,11 @@
 /*=============== HOME SWIPER ===============*/
+const homeSwiper = new Swiper('.home-swiper', {
+	loop: true,
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+});
 
 /*=============== PRODUCT SWIPER ===============*/
 
