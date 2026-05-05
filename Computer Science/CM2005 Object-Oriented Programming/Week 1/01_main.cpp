18b92191270c0be282d5e405b6fe09370a33c80d
tsimeonov <tsimeonov12@gmail.com>
2026-05-05 19:26:42 +0200

    style

---
+++ Computer Science/CM2005 Object-Oriented Programming/Week 1/01_main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+int main()
+{
+
+  while (true)
+  {
+    std::cout << "1: Print help" << std::endl;
+    std::cout << "2: Print exchange stats" << std::endl;
+    std::cout << "3: Place an ask" << std::endl;
+    std::cout << "4: Place a bid" << std::endl;
+    std::cout << "5: Print wallet" << std::endl;
+    std::cout << "6: Continue" << std::endl;
+
+    int userOption;
+    std::cout << "Type in 1-6" << std::endl;
+    std::cin >> userOption;
+
+    if (userOption == 1)
+    {
+      std::cout << "Help - choose options from the menu" << std::endl;
+      std::cout << "and follow the on screen instructions" << std::endl;
+    }
+    if (userOption == 2)
+    {
+      std::cout << "Market looks good" << std::endl;
+    }
+    if (userOption == 3)
+    {
+      std::cout << "Make an offer - enter amount" << std::endl;
+    }
+    if (userOption == 4)
+    {
+      std::cout << "Make a bid - enter amount" << std::endl;
+    }
+    if (userOption == 5)
+    {
+      std::cout << "Yot wallet is empty" << std::endl;
+    }
+    if (userOption == 6)
+    {
+      std::cout << "Going to next time frame" << std::endl;
+    }
+    if (userOption < 1 || userOption > 6)
+    {
+      std::cout << "Invalid choice. Choose 1 -6" << std::endl;
+    }
+  }
+}
