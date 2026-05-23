19a65375016a7f48dd12936ab61c227694930268
tsimeonov <tsimeonov12@gmail.com>
2026-05-23 17:05:15 +0200

    update

---
+++ Practice/app.js
@@ -0,0 +1,25 @@
+/*
+a module is a self-contained unit of code that encapsulated related functions data and behaviour
+
+a module is a self-contained unit of code that encapsulated similar functions data and behaviour
+
+A module is a self-contained unit of code that encapsulates related functions data and behaviour.
+
+A module is a self-contained unit of code that encapsulated similar functions data and behaviour
+
+module complexity measures how dificult a module is to understand, test, debug and modify
+
+module complexity meausres how dificutlt a module is to understandm test , debug and modify
+It's influenced by foctors like
+- size
+- control flow complexity
+- dependencies on other modules
+- data structures used internaly
+
+A module complexity measures how difficult a module is to understand, test, degug and modify.
+It's influenced by factors like:
+- size
+- control flow complexity
+- dependicies on other modules
+
+*/
