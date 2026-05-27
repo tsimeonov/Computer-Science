c5c82c6651cfc84809bc7fd3df7731492ac7585c
tsimeonov <tsimeonov12@gmail.com>
2026-05-27 20:17:39 +0200

    add js

--- JS Projects/50_Ecommerce Website/assets/js/accordion.js
+++ JS Projects/50_Ecommerce Website/assets/js/accordion.js
@@ -1,1 +1,15 @@
 /*=============== ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+accordionItems.forEach((item) => {
+	const header = item.querySelector('.accordion-header');
+	const content = item.querySelector('.accordion-content');
+	const icon = item.querySelector('.accordion-header i');
+
+	header.addEventListener('click', () => {
+		const isOpen = item.classList.toggle('accordion-open');
+
+		content.style.height = isOpen ? content.scrollHeight + 'px' : '0';
+		icon.className = isOpen ? 'ri-subtract-line' : 'ri-add-line';
+	});
+});
