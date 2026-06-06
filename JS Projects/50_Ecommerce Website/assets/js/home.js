03074aef12abc7c31efb32f170088f294d5893b1
tsimeonov <tsimeonov12@gmail.com>
2026-06-06 14:16:28 +0200

    update

--- JS Projects/50_Ecommerce Website/assets/js/home.js
+++ JS Projects/50_Ecommerce Website/assets/js/home.js
@@ -10,12 +10,29 @@ const homeSwiper = new Swiper('.home-swiper', {
 /*=============== PRODUCT SWIPER ===============*/
 const productSwiper = new Swiper('.product-swiper', {
 	loop: true,
-	slidesPerView: 3,
-	spaceBetween: 30,
+
+	breakpoints: {
+		640: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		768: {
+			slidesPerView: 4,
+			spaceBetween: 40,
+		},
+		1200: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+		1400: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+		},
+	},
 });
 
 /*=============== ELEVATE SWIPER ===============*/
-const productsSwiper = new Swiper('.product-elevate-swiper', {
+const elevateSwiper = new Swiper('.product-elevate-swiper', {
 	loop: true,
 	// If we need pagination
 	pagination: {
