c861cf9713ee4449c9df590c0b795b59e36c3815
tsimeonov <tsimeonov12@gmail.com>
2026-06-11 19:17:14 +0200

    Update example.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Week 3/example.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Week 3/example.cpp
@@ -3,14 +3,5 @@
 int main()
 {
 
-  unsigned short i = 65534;
-  signed long l = 234234;
-
-  std::cout << "The size of the unsigned is: " << sizeof(i) << std::endl;
-  std::cout << "i contains " << i << std::endl;
-
-  std::cout << "The size of the signed is: " << sizeof(l) << std::endl;
-  std::cout << "l contains " << l << std::endl;
-
   return 0;
 }
