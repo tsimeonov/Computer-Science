1ad3db1cffa08744d7714360f5d4381f95fd9792
tsimeonov <tsimeonov12@gmail.com>
2026-06-11 19:21:17 +0200

    update

--- Computer Science/CM2005 Object-Oriented Programming/Week 3/example.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Week 3/example.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <string>
+
+enum class Colours
+{
+  blue,
+  green,
+  red
+};
 
 int main()
 {
+
+  std::string s = "Hello";
+
+  std::cout << s << std::endl;
 
   return 0;
 }
