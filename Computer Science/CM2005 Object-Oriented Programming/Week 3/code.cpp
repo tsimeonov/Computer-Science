fb92e2a72c4535d753d6370016ecc57a50b9784a
tsimeonov <tsimeonov12@gmail.com>
2026-06-13 10:49:55 +0200

    update

--- Computer Science/CM2005 Object-Oriented Programming/Week 3/code.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Week 3/code.cpp
@@ -15,19 +15,25 @@ enum class OrderBookType
 class OrderBookEntry
 {
 public:
-  OrderBookEntry(double price,
-                 double amount,
-                 std::string timestamp,
-                 std::string product,
-                 OrderBookType orderType)
+  OrderBookEntry(double _price,
+                 double _amount,
+                 std::string _timestamp,
+                 std::string _product,
+                 OrderBookType _orderType)
   {
+    // this->price = price;
+    price = _price;
+    amount = _amount;
+    timestamp = _timestamp;
+    product = _product;
+    orderType = _orderType;
   }
 
   double price;
   double amount;
   std::string timestamp;
   std::string product;
-  OrderBookType orderTypex;
+  OrderBookType orderType;
 };
 
 void printMenu()
