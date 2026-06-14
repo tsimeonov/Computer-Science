071cecf867850145fc94e446997ea2052c949d69
tsimeonov <tsimeonov12@gmail.com>
2026-06-14 15:26:06 +0200

    update

---
+++ Computer Science/CM2005 Object-Oriented Programming/Week 3/MerkelMain.cpp
@@ -0,0 +1,15 @@
+#include "MerkelMain.h"
+#include <iostream>
+
+void MerkelMain::printMenu()
+{
+  std::cout << "=====================" << std::endl;
+  std::cout << "1: Print help" << std::endl;
+  std::cout << "2: Print exchange stats" << std::endl;
+  std::cout << "3: Place an ask" << std::endl;
+  std::cout << "4: Place a bid" << std::endl;
+  std::cout << "5: Print wallet" << std::endl;
+  std::cout << "6: Continue" << std::endl;
+
+  std::cout << "=====================" << std::endl;
+}
---
+++ Computer Science/CM2005 Object-Oriented Programming/Week 3/MerkelMain.h
@@ -0,0 +1,14 @@
+class MerkelMain
+{
+public:
+  MerkelMain();
+  void printMenu();
+  void printHelp();
+  void printMarketStats();
+  void enterOffer();
+  void enterBid();
+  void printWallet();
+  void gotoNextTimeFrame();
+  int getUserOption();
+  void processUserOption(int userOption);
+};
