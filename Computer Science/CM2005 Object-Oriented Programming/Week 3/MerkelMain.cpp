67f3ec384f3ea13b139692e0d7742ed14c39bcc1
tsimeonov <tsimeonov12@gmail.com>
2026-06-14 15:29:54 +0200

    update

--- Computer Science/CM2005 Object-Oriented Programming/Week 3/MerkelMain.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Week 3/MerkelMain.cpp
@@ -13,3 +13,77 @@ void MerkelMain::printMenu()
 
   std::cout << "=====================" << std::endl;
 }
+
+void MerkelMain::printHelp()
+{
+  std::cout << "Help - choose options from the menu" << std::endl;
+  std::cout << "and follow the on screen instructions" << std::endl;
+}
+
+void MerkelMain::printMarketStats()
+{
+  std::cout << "Market looks good" << std::endl;
+}
+
+void MerkelMain::enterOffer()
+{
+  std::cout << "Make an offer - enter amount" << std::endl;
+}
+
+void MerkelMain::enterBid()
+{
+  std::cout << "Make a bid - enter amount" << std::endl;
+}
+
+void MerkelMain::printWallet()
+{
+  std::cout << "Yot wallet is empty" << std::endl;
+}
+
+void MerkelMain::gotoNextTimeFrame()
+{
+  std::cout << "Going to next time frame" << std::endl;
+}
+
+int MerkelMain::getUserOption()
+{
+  int userOption;
+
+  std::cout << "Type in 1-6" << std::endl;
+  std::cin >> userOption;
+  std::cout << "You chose: " << userOption << std::endl;
+
+  return userOption;
+}
+
+void MerkelMain::processUserOption(int userOption)
+{
+  if (userOption == 1)
+  {
+    printHelp();
+  }
+  if (userOption == 2)
+  {
+    printMarketStats();
+  }
+  if (userOption == 3)
+  {
+    enterOffer();
+  }
+  if (userOption == 4)
+  {
+    enterBid();
+  }
+  if (userOption == 5)
+  {
+    printWallet();
+  }
+  if (userOption == 6)
+  {
+    gotoNextTimeFrame();
+  }
+  if (userOption < 1 || userOption > 6)
+  {
+    std::cout << "Invalid choice. Choose 1 -6" << std::endl;
+  }
+}
