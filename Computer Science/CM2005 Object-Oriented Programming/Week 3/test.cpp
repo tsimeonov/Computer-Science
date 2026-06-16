0b143071bcc1f66a9afc2d3c4583e4caf6e94eb2
tsimeonov <tsimeonov12@gmail.com>
2026-06-16 19:52:40 +0200

    Update test.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Week 3/test.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Week 3/test.cpp
@@ -4,7 +4,28 @@
 
 std::vector<std::string> tokenise(std::string csvLine, char separator)
 {
+  // string vector tokens ## stores the tokens
   std::vector<std::string> tokens;
+
+  // char separator = "," ## the character that separates the tokens
+
+  // int start, end ## used to delineate the position of the tokens
+
+  // start = csvLine.find_first_not_of(setarator)
+  // do
+  // end = next "separator" after start
+  //   if start == csvLine.length or start == end ## noting more to find
+  //     break
+  //   if end >= 0 # we found the separator
+  //     token  csvLine.substr(start, end - start) ## start, substring length
+  //   else
+  //     token = scvLine.substr(start, csvLine.length - start) ## end is invalid
+
+  //   token.push_back(token) ## save the token
+  //   start = end + 1 ## move past this token
+
+  // while (end > 0) ## continue loop condition
+
   return tokens;
 }
 
