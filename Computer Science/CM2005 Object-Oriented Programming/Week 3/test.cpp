6c7f1a0169f1d2577032e10c08c24bd55d7d4c25
tsimeonov <tsimeonov12@gmail.com>
2026-06-17 19:16:29 +0200

    Update test.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Week 3/test.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Week 3/test.cpp
@@ -13,18 +13,26 @@ std::vector<std::string> tokenise(std::string csvLine, char separator)
   // start = csvLine.find_first_not_of(setarator)
   start = csvLine.find_first_not_of(separator, 0);
   // do
-  // end = next "separator" after start
-  //   if start == csvLine.length or start == end ## noting more to find
-  //     break
-  //   if end >= 0 # we found the separator
-  //     token  csvLine.substr(start, end - start) ## start, substring length
-  //   else
-  //     token = scvLine.substr(start, csvLine.length - start) ## end is invalid
+  do
+  {
+    // end = next "separator" after start
+    end = csvLine.find_first_of(separator, start);
+    //   if start == csvLine.length or start == end ## noting more to find
+    if (start == csvLine.length() || start == end) // break
+    //     break
+    //   if end >= 0 # we found the separator
+    //     token  csvLine.substr(start, end - start) ## start, substring length
+    //   else
+    //     token = scvLine.substr(start, csvLine.length - start) ## end is invalid
 
-  //   token.push_back(token) ## save the token
-  //   start = end + 1 ## move past this token
-
+    //   token.push_back(token) ## save the token
+    //   start = end + 1 ## move past this token
+  }
   // while (end > 0) ## continue loop condition
+  while (true)
+  {
+    /* code */
+  }
 
   return tokens;
 }
