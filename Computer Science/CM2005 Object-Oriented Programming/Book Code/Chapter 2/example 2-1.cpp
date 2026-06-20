c24c580242c058c5980a831514b0bf32bf19520b
tsimeonov <tsimeonov12@gmail.com>
2026-06-20 16:01:44 +0200

    update

---
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Chapter 2/example 2-1.cpp
@@ -0,0 +1,18 @@
+// Given the length and width of a rectangle, this c+ program
+// computes and outputs the perimeter and area of the rectangle
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  double length;
+  double width;
+  double area;
+  double perimeter;
+
+  cout << "Program to compute and output the perimeter and " << "area of a rectangle" << endl;
+
+  return 0;
+}
