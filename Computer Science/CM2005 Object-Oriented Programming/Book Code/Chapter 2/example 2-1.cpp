4115440c735f72cb957e7ed52a4aa5f3ee8c9942
tsimeonov <tsimeonov12@gmail.com>
2026-06-20 16:05:08 +0200

    update program

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Chapter 2/example 2-1.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Chapter 2/example 2-1.cpp
@@ -14,5 +14,15 @@ int main()
 
   cout << "Program to compute and output the perimeter and " << "area of a rectangle" << endl;
 
+  length = 6.0;
+  width = 4.0;
+  perimeter = 2 * (length + width);
+  area = length * width;
+
+  cout << "Lenght = " << length << endl;
+  cout << "Width = " << width << endl;
+  cout << "Perimeter = " << perimeter << endl;
+  cout << "Area = " << area << endl;
+
   return 0;
 }
