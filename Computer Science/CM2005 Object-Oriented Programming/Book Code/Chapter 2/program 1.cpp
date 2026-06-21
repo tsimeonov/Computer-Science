5b831ddb16d47088fa1dffc8785bde7afcb8a032
tsimeonov <tsimeonov12@gmail.com>
2026-06-21 12:54:28 +0200

    add comments

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Chapter 2/program 1.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Chapter 2/program 1.cpp
@@ -1,4 +1,9 @@
+/*
+CONVERT LENGHT
 
+Write a program that takes as input given lenghts expressed in feet and inches.
+The program should then convert and output the lengths in centimeters.
+*/
 #include <iostream>
 
 int main()
@@ -14,6 +19,20 @@ int main()
 
   const double CENTIMETERS_PER_INCH = 2.54;
   const int INCHES_PER_FOOT = 12;
+
+  // 1. Prompt the user for the input
+
+  // 2. Get the data
+
+  // 3. Echo the input - that is, output what the program read as input
+
+  // 4. Find the lenght in inches
+
+  // 5. Ouput the length in inches
+
+  // 6. Convert the length to centimeters
+
+  // 7. Output the length in centimeters
 
   return 0;
 }
