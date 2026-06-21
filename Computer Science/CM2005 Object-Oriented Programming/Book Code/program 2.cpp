2ed7a51c74cca0c38589eac69c53dc7a90593d41
tsimeonov <tsimeonov12@gmail.com>
2026-06-21 13:56:10 +0200

    Create program 2.cpp

---
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/program 2.cpp
@@ -0,0 +1,34 @@
+/*
+Make Change
+
+Write a program that takes as input any change expressed in cents.
+It should then compute the number of half-dollars, quarters, nickels and pennies in that order.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  // 1. Get the change in cents
+
+  // 2. Find the number of half-dollars
+
+  // 3. Calculate the remaining change
+
+  // 4. Find the number of quarters.
+
+  // 5. Calculate the remaining change
+
+  // 6. Find the number of dimes
+
+  // 7. Calculate the remaining change
+
+  // 8. Find the number of nickels
+
+  // 9. Calculate the remaining change, which is the number of pennies
+
+  return 0;
+}
