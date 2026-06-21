3950ab73b6e537c7b078d9e55b9edb51463dbf5c
tsimeonov <tsimeonov12@gmail.com>
2026-06-21 13:49:41 +0200

    Update program 1_book.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/program 1_book.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/program 1_book.cpp
@@ -9,4 +9,23 @@ const int INCHES_PER_FOOT = 12;
 
 int main()
 {
+  // declare variables
+  int feet, inches;
+  int totalInches;
+  double centimeters;
+
+  // Statements: Step 1 - Step 7
+  cout << "Enter two integers, one for feet and " << "one for inches:";
+  cin >> feet >> inches;
+  cout << endl;
+
+  cout << "The numbers you entered are " << feet << " for feet and " << inches << " for inches " << endl;
+
+  totalInches = INCHES_PER_FOOT * feet + inches;
+
+  cout << "The toal number of inches = " << totalInches << endl;
+
+  centimeters = CENTIMETERS_PER_INCH * totalInches;
+
+  return 0;
 }
