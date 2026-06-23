9940069456584e4a305762853c5c611746e9dcb6
tsimeonov <tsimeonov12@gmail.com>
2026-06-23 19:22:12 +0200

    exercise 2

---
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 1/exercise 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "cccccccc          ++                 ++" << endl;
+  cout << "cc                ++                 ++" << endl;
+  cout << "cc           +++++++++++++     +++++++++++++" << endl;
+  cout << "cc           +++++++++++++     +++++++++++++" << endl;
+  cout << "cc           +++++++++++++     +++++++++++++" << endl;
+  cout << "cccccccc          ++                 ++" << endl;
+
+  return 0;
+}
