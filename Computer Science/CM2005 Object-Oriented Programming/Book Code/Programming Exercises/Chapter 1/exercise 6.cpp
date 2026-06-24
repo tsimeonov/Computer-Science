329ecb149192bbf7bd8a1a2e5227ddc03a3e04c0
tsimeonov <tsimeonov12@gmail.com>
2026-06-24 20:12:04 +0200

    Update exercise 6.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 1/exercise 6.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 1/exercise 6.cpp
@@ -9,3 +9,8 @@ int main()
 
   // return statement
 }
+
+// 1. Write a C++ statement that includes the header files iostream and string
+// 2. Write a C++ statement that allows you to use cin, cout and endl without the
+// prefic std::.
+// 3. Write C++ statement that declare the follwoing variables: name of type string and studyHours of type double
