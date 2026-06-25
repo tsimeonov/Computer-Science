d18b0b4e235856ec64203afdd8b0c8c1be004c05
tsimeonov <tsimeonov12@gmail.com>
2026-06-25 19:38:32 +0200

    Update exercise 6.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 1/exercise 6.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 1/exercise 6.cpp
@@ -12,15 +12,23 @@ int main()
   double studyHours;
 
   // executable statements
+  cout << "What is yout name: " << endl;
+  cin >> name;
+
+  cout << "How many hours do you need to study: " << endl;
+  cin >> studyHours;
+
+  cout << "Hello, " << name << "! on Saturday, you need to study 4.5 hrs. for the exam" << endl;
 
   // return statement
+  return 0;
 }
 
 // 1. Write a C++ statement that includes the header files iostream and string
 // 2. Write a C++ statement that allows you to use cin, cout and endl without the
 // prefic std::.
 // 3. Write C++ statement that declare the follwoing variables: name of type string and studyHours of type double
-// 4. Write a C++ statements that prompt tand input a string into name and double value into studyHours
+// 4. Write a C++ statements that prompt and input a string into name and double value into studyHours
 // 5. Write a C++ statement that outputs the values of name and studyHours
 // with the appropriate text. For example, if the value of name is "Donald" and
 // the value of studyHours is 4.5 the output is:
