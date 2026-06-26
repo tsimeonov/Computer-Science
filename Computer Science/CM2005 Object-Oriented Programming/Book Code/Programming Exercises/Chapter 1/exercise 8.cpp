bea1d4ab9b2eed0a9d26b80ff8a727a3f5902f95
tsimeonov <tsimeonov12@gmail.com>
2026-06-26 20:14:42 +0200

    Update exercise 8.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 1/exercise 8.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 1/exercise 8.cpp
@@ -8,3 +8,8 @@ int main()
   // executable statements
   // return statement
 }
+
+// A) Write a C++ statements that include the header files iostream and string
+// B) write a C++ statement that allows you to use cin, cout and endl withiut the prefix std::
+// C) write a C++ statements that declare and initialize the following named
+// constaints: SECRET of type int initialized to 11 and RATE of type double initialized to 12.50
