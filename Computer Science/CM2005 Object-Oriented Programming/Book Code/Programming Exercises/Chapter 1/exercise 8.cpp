d57399ee589c4f6be1ab3151b2f987d293979ad2
tsimeonov <tsimeonov12@gmail.com>
2026-06-27 13:52:10 +0200

    Update exercise 8.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 1/exercise 8.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 1/exercise 8.cpp
@@ -13,3 +13,19 @@ int main()
 // B) write a C++ statement that allows you to use cin, cout and endl withiut the prefix std::
 // C) write a C++ statements that declare and initialize the following named
 // constaints: SECRET of type int initialized to 11 and RATE of type double initialized to 12.50
+// D) write a C++ statements that declare the following variables: num1, num2 and newNum of type int; name of type string; and hoursWorked and wages of type double
+// E) write a C++ statements that prompt the user to input two integers and store the first number in num1 and the second number in num2.
+// F) write C++ statement that outputs the values of num1 and num2, indicating which is num2.
+// for example, if num1 is 8 and num2 is 5 then the putput is:
+// The value of num1 = 8 and the value if num2 = 5
+// G) write a C++ statement that multiplies the value of num1 by 2, adds the value of num2 to it, and then stores the result in newNum.
+// H) write a C++ statement that updates the value of newNum by adding the value of the named constant SECRET to it. Then, write a C++ statement that outputs the value of newNum with an appropriate message
+// I) write C++ statements that prompt the user to enter a person's last name and then store the last name into the variable name
+// J) write C++ statements that prompt the user to enter a decimal number between 0 and 70 and then store the number entered into hoursWorked.
+// K) write a C+ statement that multiplies the value of the named constatant RATE with the value of hoursWorked and then stores the result into the variable wages.
+// I) write C++ statement that produce the following output:
+// Name:  // output the value of the variable name
+// Pay Rate: $  // output the value of the RATE
+// Hous Worked:  // output the value of the variable hoursWorked
+// Salary: $  // output the value of the variable wages
+// M) write a C++ program that tests each of the C++ statements that you wrote in parts a through I.
