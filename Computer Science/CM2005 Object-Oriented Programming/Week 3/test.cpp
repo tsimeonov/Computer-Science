c91e85a266b7d766980ce7eb3c5ba8433c54550f
tsimeonov <tsimeonov12@gmail.com>
2026-06-30 19:38:07 +0200

    update

--- Computer Science/CM2005 Object-Oriented Programming/Week 3/test.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Week 3/test.cpp
@@ -80,9 +80,18 @@ int main()
       }
 
       // we have 5 tokens
-      double price = std::stod(tokens[3]);
-      double amount = std::stod(tokens[4]);
-      std::cout << price << ":" << amount << std::endl;
+      try
+      {
+        double price = std::stod(tokens[3]);
+        double amount = std::stod(tokens[4]);
+        std::cout << price << ":" << amount << std::endl;
+      }
+      catch (std::exception &e)
+      {
+        std::cout << "Bad float!" << tokens[3] << std::endl;
+        std::cout << "Bad float!" << tokens[4] << std::endl;
+        continue;
+      }
     }
 
     csvFile.close();
