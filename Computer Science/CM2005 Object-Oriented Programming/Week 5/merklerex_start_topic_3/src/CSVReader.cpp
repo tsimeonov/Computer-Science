8662d59749cc77b91c92de53606612469d001e5b
tsimeonov <tsimeonov12@gmail.com>
2026-07-01 20:04:12 +0200

    Update CSVReader.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Week 5/merklerex_start_topic_3/src/CSVReader.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Week 5/merklerex_start_topic_3/src/CSVReader.cpp
@@ -8,7 +8,19 @@ CSVReader::CSVReader()
 
 vector<OrderBookEntry> CSVReader::readCSV(string csvFile)
 {
+  vector<OrderBookEntry> entries;
+  return entries;
 }
 
-static vector<string> CSVReader::tokenise(string csvLine, char separator);
-OrderBookEntry CSVReader::stringToOBE(vector<string> strings);
+vector<string> CSVReader::tokenise(string csvLine, char separator)
+{
+  vector<string> tokens;
+  return tokens;
+}
+
+OrderBookEntry CSVReader::stringToOBE(vector<string> strings)
+{
+  OrderBookEntry obe{1, 1, "", "", OrderBookType::bid};
+
+  return obe;
+}
