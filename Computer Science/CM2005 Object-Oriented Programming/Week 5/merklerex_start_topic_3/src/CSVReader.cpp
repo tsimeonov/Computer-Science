cde0946bc88808468bc1ebec8d1b729d78dff0de
tsimeonov <tsimeonov12@gmail.com>
2026-07-02 19:59:22 +0200

    Update CSVReader.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Week 5/merklerex_start_topic_3/src/CSVReader.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Week 5/merklerex_start_topic_3/src/CSVReader.cpp
@@ -18,8 +18,28 @@ vector<string> CSVReader::tokenise(string csvLine, char separator)
   return tokens;
 }
 
-OrderBookEntry CSVReader::stringToOBE(vector<string> strings)
+OrderBookEntry CSVReader::stringToOBE(vector<string> tokens)
 {
+
+  if (tokens.size() != 5) // bad
+  {
+    cout << "Bad line" << endl;
+    continue;
+  }
+  // We have 5 tokens
+  try
+  {
+    double price = stod(tokens[3]);
+    double price = stod(tokens[4]);
+    cout << price << ":" << amount << endl;
+  }
+  catch (exception &e)
+  {
+    cout << "Bad float! " << tokens[3] << endl;
+    cout << "Bad float! " << tokens[4] << endl;
+    break;
+  }
+
   OrderBookEntry obe{1, 1, "", "", OrderBookType::bid};
 
   return obe;
