db8e796f121844030ea1fbb51f7e96cea5178abe
tsimeonov <tsimeonov12@gmail.com>
2026-07-04 12:58:41 +0200

    Update CSVReader.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Week 5/merklerex_start_topic_3/src/CSVReader.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Week 5/merklerex_start_topic_3/src/CSVReader.cpp
@@ -14,38 +14,28 @@ vector<OrderBookEntry> CSVReader::readCSV(string csvFile)
 
 vector<string> CSVReader::tokenise(string csvLine, char separator)
 {
-  // string vector tokens ## stores the tokens
-  std::vector<std::string> tokens;
 
-  // int start, end ## used to delineate the position of the tokens
+  std::vector<std::string> tokens;
   signed int start, end;
   std::string token;
-
-  // start = csvLine.find_first_not_of(setarator)
   start = csvLine.find_first_not_of(separator, 0);
-  // do
   do
   {
-    // end = next "separator" after start
     end = csvLine.find_first_of(separator, start);
-    // if start == csvLine.length or start == end ## noting more to find
     if (start == csvLine.length() || start == end)
-      // break
+
       break;
-    // if end >= 0 # we found the separator
     if (end >= 0)
       token = csvLine.substr(start, end - start);
-    // token  csvLine.substr(start, end - start) ## start, substring length
-    // else
+
     else
       token = csvLine.substr(start, csvLine.length() - start);
-    // token = csvLine.substr(start, csvLine.length - start) ## end is invalid
-    // token.push_back(token) ## save the token
+
     tokens.push_back(token);
-    // start = end + 1 ## move past this token
+
     start = end + 1;
   }
-  // while (end > 0) ## continue loop condition
+
   while (end != std::string::npos);
 
   return tokens;
