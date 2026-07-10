cca6c8dbba87da30574ff58c2edfe0e0eb394a18
tsimeonov <tsimeonov12@gmail.com>
2026-07-10 20:19:20 +0200

    Update OrderBook.h

--- Computer Science/CM2005 Object-Oriented Programming/Week 5/merklerex_start_topic_3/src/OrderBook.h
+++ Computer Science/CM2005 Object-Oriented Programming/Week 5/merklerex_start_topic_3/src/OrderBook.h
@@ -9,8 +9,8 @@ class OrderBook
 {
 
 public:
-  // construct, reading a csv data file
-  OrderBook(string fileName);
+  construct, reading a csv data file
+             OrderBook(string fileName);
   // return vector of all known products in the dataset
   vector<string> getKnownProducts();
   // return vector of Orders according to the sent filters
