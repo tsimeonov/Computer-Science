d12b9a6a9594fccbd32d3fbb527624714ae3930a
tsimeonov <tsimeonov12@gmail.com>
2026-07-15 20:25:25 +0200

    Update test.js

--- Computer Science/CM2010 Software Design and Development/Midterm/Part 2/test.js
+++ Computer Science/CM2010 Software Design and Development/Midterm/Part 2/test.js
@@ -11,9 +11,9 @@ function point_is_in_box(
 	return true;
 }
 
-describe('Collision Detection Tests', function () {
-	it('should return true when the point is inside the the box', function () {
-		const result = point_is_in_box(5, 5, 0, 0, 10, 10);
-		assert.strictEqual(result, true);
-	});
-});
+// describe('Collision Detection Tests', function () {
+// 	it('should return true when the point is inside the the box', function () {
+// 		const result = point_is_in_box(5, 5, 0, 0, 10, 10);
+// 		assert.strictEqual(result, true);
+// 	});
+// });
