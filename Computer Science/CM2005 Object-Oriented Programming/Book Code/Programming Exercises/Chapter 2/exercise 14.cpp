8ecb4f4b7a322a2f16778d8c120bdfcf0e092575
tsimeonov <tsimeonov12@gmail.com>
2026-07-18 14:14:15 +0200

    update

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 14.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 14.cpp
@@ -15,5 +15,14 @@ int main()
   float exaByte = petaByte * (1 << 10);
   float zettaByte = exaByte * (1 << 10);
 
+  // create a separate variable just for the user's input
+  float userInput;
+
+  cout << "Type storage in GB" << endl;
+  cin >> userInput;
+
+  float totalBytes = userInput * gigaByte;
+  cout << "The storage in GB is: " << totalBytes << endl;
+
   return 0;
 }
