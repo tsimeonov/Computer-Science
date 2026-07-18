e3da5310433f98320d9d2e05c51f79ab828d4d48
tsimeonov <tsimeonov12@gmail.com>
2026-07-18 14:23:16 +0200

    clear

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 14.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 14.cpp
@@ -1,28 +1,10 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-
-  // variables
-  float byte;
-  float kiloByte = 1 << 10;
-  float megaByte = kiloByte * (1 << 10);
-  float gigaByte = megaByte * (1 << 10);
-  float teraByte = gigaByte * (1 << 10);
-  float petaByte = teraByte * (1 << 10);
-  float exaByte = petaByte * (1 << 10);
-  float zettaByte = exaByte * (1 << 10);
-
-  // create a separate variable just for the user's input
-  float userInput;
-
-  cout << "Type storage in GB" << endl;
-  cin >> userInput;
-
-  float totalBytes = userInput * gigaByte;
-  cout << "The storage in GB is: " << totalBytes << endl;
 
   return 0;
 }
