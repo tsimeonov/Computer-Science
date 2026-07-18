e51ebd735d17ab99944cfdc3b1a97c0be72a8a08
tsimeonov <tsimeonov12@gmail.com>
2026-07-18 15:16:52 +0200

    update

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 16.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 16.cpp
@@ -5,5 +5,29 @@ using namespace std;
 int main()
 {
 
+  double oneMilkCarton = 3.78;
+  double totalMilk;
+  int totalMilkCartons;
+  double oneLiterMilk = 0.38;
+  double totalMilkProd;
+  double profit = 0.27;
+  double totalProfit;
+
+  cout << "What is the total amount produced in the morning" << endl;
+  cin >> totalMilk;
+
+  totalMilkCartons = totalMilk / oneMilkCarton;
+  cout
+      << totalMilkCartons << " cartons are needed to hold milk" << endl;
+
+  // Output the cost of producing the milk
+
+  totalMilkProd = totalMilkCartons * oneLiterMilk;
+  cout << "The cost to produce milk is " << totalMilkProd << endl;
+
+  // Output the profit for prodicng milk
+  totalProfit = profit * totalMilkCartons;
+  cout << "The profit is: " << totalProfit << endl;
+
   return 0;
 }
