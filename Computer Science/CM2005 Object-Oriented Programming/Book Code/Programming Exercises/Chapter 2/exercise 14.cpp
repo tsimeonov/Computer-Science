5b36b187cec23b823b5e4a443901db2d8ee9c206
tsimeonov <tsimeonov12@gmail.com>
2026-07-18 14:35:48 +0200

    Update exercise 14.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 14.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 14.cpp
@@ -7,25 +7,17 @@ int main()
 {
 
   // 1. Declare variables to store out numbers
-  float advertiseGB;
-  float totalBytes;
-  float actualGB;
 
   // 2. Prompt the user for the hard drive size
   cout << "Enter the size of the hard drive specified by the manufacturer (in GB): " << endl;
-  cin >> advertiseGB;
 
   // 3. Convert the manufacturers GB into total bytes (Base-10)
   // 1GB = 1000 * 1000 * 1000;
-  totalBytes = advertiseGB * (1000 * 1000 * 1000);
 
   // 4. Convert those total bytes into true computer BG (base-2)
   // 1BG = 1024 * 1024 * 1024;
-  actualGB = totalBytes / (1024 * 1024 * 1024);
 
   // 5. Output the final calculation result
-  cout << fixed << setprecision(2);
-  cout << "The actual storage capacity is: " << actualGB << " GB" << endl;
 
   return 0;
 }
