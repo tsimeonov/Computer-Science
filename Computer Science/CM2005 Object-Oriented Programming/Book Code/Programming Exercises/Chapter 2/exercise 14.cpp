8d781145fb115e542d2ac7d28b9f996fb09913da
tsimeonov <tsimeonov12@gmail.com>
2026-07-18 13:56:31 +0200

    add variables

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 14.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 14.cpp
@@ -2,4 +2,18 @@
 
 using namespace std;
 
-int main() {}
+int main()
+{
+
+  // variables
+  float byte;
+  float kiloByte = 1 << 10;
+  float megaByte = kiloByte * (1 << 10);
+  float gigaByte = megaByte * (1 << 10);
+  float teraByte = gigaByte * (1 << 10);
+  float petaByte = teraByte * (1 << 10);
+  float exaByte = petaByte * (1 << 10);
+  float zettaByte = exaByte * (1 << 10);
+
+  return 0;
+}
