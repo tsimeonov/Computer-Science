b5daeb4ec9f8c5ca1e115365b35f2aae302c1d4b
tsimeonov <tsimeonov12@gmail.com>
2026-07-19 15:26:55 +0200

    update

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 18.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 18.cpp
@@ -4,5 +4,35 @@ using namespace std;
 
 int main()
 {
+  // Varialbles
+  double hourRate;
+  double taxRate = 0.14;
+  double hoursWorked;
+  // Money you receive after taxess
+  double netIncome;
+  double grossIncome;
+  double incomeAfterTaxes;
+  double clothes = 10;
+  double supplies = 1;
+
+  // Enter pay rate per hour
+  cout << "Enter the hour rate" << endl;
+  cin >> hourRate;
+
+  // Number of hours worked in a week
+  cout << "Number of hours worked in a week" << endl;
+  cin >> hoursWorked;
+
+  // A: Your income before and after taxes
+  grossIncome = hourRate * hoursWorked;
+  cout << "Your gross income is: $" << grossIncome << endl;
+
+  netIncome = grossIncome * taxRate;
+  incomeAfterTaxes = grossIncome - netIncome;
+  cout << "Your net income is: $" << incomeAfterTaxes << endl;
+
+  // cout << "Hour rate: " << hourRate << endl;
+  // cout << "Houurs worked " << hoursWorked << endl;
+
   return 0;
 }
