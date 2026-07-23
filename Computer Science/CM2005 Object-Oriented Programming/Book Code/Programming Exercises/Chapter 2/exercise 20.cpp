765e9274affd54bb146e5cff04a8b9a560dadab8
tsimeonov <tsimeonov12@gmail.com>
2026-07-23 20:06:11 +0200

    Update exercise 20.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 20.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 20.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main()
 {
+
+  cout << "Please enter your fixed commision" << endl;
+  cout << "Please enter your percentage of the commission" << endl;
+  cout << "Please enter the purchase price of the car" << endl;
+  cout << "Please enter minimum price added" << endl;
+  cout << "Please enter maximum price added" << endl;
+
   return 0;
 }
