76a5c85390eb897bc16192f509faf256aba7a989
tsimeonov <tsimeonov12@gmail.com>
2026-07-23 19:52:33 +0200

    Update exercise 19.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 19.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 19.cpp
@@ -5,5 +5,22 @@ using namespace std;
 int main()
 {
 
+  // quarters to pennies
+  // dimes to pennies
+  // nickels to pennies
+
+  double pennies;
+  double dimes;
+  double nickels;
+
+  cout << "Enter pennies: " << endl;
+  cin >> pennies;
+
+  cout << "Enter dimes: " << endl;
+  cin >> dimes;
+
+  cout << "Enter nickesl: " << endl;
+  cin >> nickels;
+
   return 0;
 }
