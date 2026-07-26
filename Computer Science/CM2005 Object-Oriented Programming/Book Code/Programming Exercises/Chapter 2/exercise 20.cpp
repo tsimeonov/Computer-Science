599476fb9c9f6b795c79daff05f2f25f10844821
tsimeonov <tsimeonov12@gmail.com>
2026-07-26 13:15:54 +0200

    update

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 20.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 20.cpp
@@ -5,17 +5,44 @@ using namespace std;
 int main()
 {
 
-  double commission;
-  double commisionPercentage;
-  double purchasePrice;
-  double maxPrice;
-  double minPrice;
+  // declare variables
+  double fixed_commission, commission_percentage, car_cost;
+  double min_added_amount, max_added_amount;
+  double min_selling_price, max_selling_price;
+  double min_commission, max_commission;
 
-  cout << "Please enter your fixed commision" << endl;
-  cout << "Please enter your percentage of the commission" << endl;
-  cout << "Please enter the purchase price of the car" << endl;
-  cout << "Please enter minimum price added" << endl;
-  cout << "Please enter maximum price added" << endl;
+  // Prompt the user for inputs
+  cout << "Enter the salespersons fixed commission (e.g., 20): " << endl;
+  cin >> fixed_commission;
+
+  cout << "Enter the commission percentage (e.g, 30)" << endl;
+  cin >> commission_percentage;
+
+  cout << "Enter the purchase const of the car:" << endl;
+  cin >> car_cost;
+
+  cout << "Enter the minimum prices addded for the selling price (e.g., 200)" << endl;
+  cin >> min_added_amount;
+
+  cout << "Enter the maximum prices addded for the selling price (e.g., 2000)" << endl;
+  cin >> max_added_amount;
+
+  // Process the calculation
+  /// Calculate selling prices
+  min_selling_price = car_cost + min_added_amount;
+  max_selling_price = car_cost + max_added_amount;
+
+  /// Calculate the commisios
+  /// Note: We divide the percentage by 100 to turn it into a decimal (e.g,. 30 / 100)
+
+  min_commission = fixed_commission + (commission_percentage / 100) * min_added_amount;
+  max_commission = fixed_commission + (commission_percentage / 100) * max_added_amount;
+
+  // Output the results
+
+  cout << "Minimum selling price of the car: $ " << min_selling_price << endl;
+  cout << "Maximum selling price of the car: $ " << max_selling_price << endl;
+  cout << "Saleasperson's commission range: $" << min_commission << " to $" << max_commission << endl;
 
   return 0;
 }
