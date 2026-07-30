52fe24ec01ee358d7651a0a0311f54f7a6f57876
tsimeonov <tsimeonov12@gmail.com>
2026-07-30 19:31:16 +0200

    update

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 22.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 2/exercise 22.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
+
+  // 1. Declare constants and variables
+  const double METRIC_TON_IN_POUNDS = 2205;
+  double bag_capacity_pounds;
+  double bags_needed;
+
+  // 2. Prompt the user for input
+  cout << "Write the amount in pounds a single bag can hold" << endl;
+  cin >> bag_capacity_pounds;
+
+  // 3. Process the calculation
+  // Divide the total metric ton weight by the capacity of a single bag
+  bags_needed = ceil(METRIC_TON_IN_POUNDS / bag_capacity_pounds);
+
+  // 4. Output the result
+  cout << "Number of bags needed to store one metric ton of rice: " << bags_needed << endl;
 
   return 0;
 }
