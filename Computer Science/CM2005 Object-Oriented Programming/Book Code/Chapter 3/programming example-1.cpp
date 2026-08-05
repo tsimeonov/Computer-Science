a6598ecda8654eb3e9506a23725520ac5234fbfc
tsimeonov <tsimeonov12@gmail.com>
2026-08-05 19:56:48 +0200

    Create programming example-1.cpp

---
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Chapter 3/programming example-1.cpp
@@ -0,0 +1,30 @@
+/*
+Program: Movie ticket sale
+This program determines the money yo be donated to a
+charity. Ot prompts the user to input the movie name, adult ticket ptice,
+child ticket price, number of adult tickets sold, number of child tickets sold,
+and percentage of the gross amount to be donated to the charity
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+  // Step 1
+  string movieName;
+  double adultTicketPrice;
+  double childTicketPrice;
+  int noOfAdultTicketSold;
+  int noOfChildTicketSold;
+  double percentDonation;
+  double grossAmount;
+  double amountDonated;
+  double netSaleAmount;
+
+  return 0;
+}
