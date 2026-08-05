903607b5c4309bde8ea6a1f79b3299170c5f3def
tsimeonov <tsimeonov12@gmail.com>
2026-08-05 19:44:44 +0200

    example 15

---
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Chapter 3/example 3-15.cpp
@@ -0,0 +1,56 @@
+// Example: Left justification
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+
+  string name = "Jessica";
+  double gpa = 3.75;
+  int scholarship = 7850;
+
+  cout << "123456789012345678901234567890" << endl;
+  cout << fixed << showpoint << setprecision(2);
+
+  cout << left;
+
+  cout << setw(10) << name
+       << setw(7) << gpa
+       << setw(8) << scholarship << endl;
+
+  cout << setfill('*')
+       << setw(10) << name
+       << setw(7) << gpa
+       << setw(8) << scholarship << endl;
+
+  cout << setw(10) << name
+       << setfill('#')
+       << setw(7) << gpa
+       << setw(8) << scholarship << endl;
+
+  cout << setw(10) << name
+       << setfill('@') << name
+       << setw(7) << setfill('#') << gpa
+       << setw(8) << setfill('^') << scholarship << endl;
+
+  cout << right;
+  cout << setfill(' ');
+  cout << setw(10) << name << setw(7) << gpa
+       << setw(8) << scholarship << endl;
+
+  return 0;
+}
+
+/*
+Sample Run:
+123456789012345678901234567890
+Jessica 3.75 7850
+Jessica***3.75***7850****
+Jessica***3.75###7850####
+Jessica@@@3.75###7850^^^^
+Jessica 3.75 7850
+*/
