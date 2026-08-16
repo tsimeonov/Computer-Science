bd932cbfa164b9a9f96284bfff695585b98bd34f
tsimeonov <tsimeonov12@gmail.com>
2026-08-16 15:05:26 +0200

    exercise 2

---
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 3/exercise 2.cpp
@@ -0,0 +1,32 @@
+#include <iomanip>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  const double PI = 3.14159;
+  double height;
+  double radius;
+
+  cout << "Enter the height of the cylinder: ";
+  cin >> radius;
+  cout << endl;
+
+  cout << "Enter the radius of the base of the cylinder: ";
+  cin >> height;
+  cout << endl;
+
+  cout << fixed << showpoint << setprecision(2);
+
+  cout << "Volume of the cylinde = "
+       << PI * pow(radius, 2.0) * height << endl;
+
+  cout << "Surface area: "
+       << 2 * PI * radius * height + 2 * PI * pow(radius, 2.0)
+       << endl;
+
+  return 0;
+}
