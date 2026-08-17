a63ff58ea9994a51791b760d463f20fcfabf9265
tsimeonov <tsimeonov12@gmail.com>
2026-08-17 07:55:57 +0200

    Update exercise 4.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 3/exercise 4.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 3/exercise 4.cpp
@@ -21,33 +21,38 @@ using namespace std;
 
 int main()
 {
-  double cost;
-  double area;
+     double cost;
+     double area;
 
-  double bagSize;
+     double bagSize;
 
-  cout << fixed << showpoint << setprecision(2);
+     cout << fixed << showpoint << setprecision(2);
 
-  cout << "Enter the amount of fertilizer, in pounds, "
-       << "in one bag: ";
-  cin >> bagSize;
-  cout << endl;
+     cout << "Enter the amount of fertilizer, in pounds, "
+          << "in one bag: ";
+     cin >> bagSize;
+     cout << endl;
 
-  cout << "Enter the cost of the " << bagSize
-       << " pound fertilizer bag: ";
-  cin >> cost;
-  cout << endl;
+     cout << "Enter the cost of the " << bagSize
+          << " pound fertilizer bag: ";
+     cin >> cost;
+     cout << endl;
 
-  cout << "Enter the area in square feet that can be "
-       << "fertilized by one bag: ";
-  cin >> area;
-  cout << endl;
+     cout << "Enter the area in square feet that can be "
+          << "fertilized by one bag: ";
+     cin >> area;
+     cout << endl;
 
-  cout << "The cost of the fertilizer per pound is: $"
-       << cost / bagSize << endl;
+     cout << "The cost of the fertilizer per pound is: $"
+          << cost / bagSize << endl;
 
-  cout << "The cost of the fertilizing per square foot is: $"
-       << cost / area << endl;
+     cout << "The cost of the fertilizing per square foot is: $"
+          << cost / area << endl;
 
-  return 0;
+     return 0;
 }
+
+/*
+
+
+*/
