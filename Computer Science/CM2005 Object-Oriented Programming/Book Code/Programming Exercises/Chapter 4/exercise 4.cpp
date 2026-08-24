800b95ecd9a1454ee8088dfb99655a34d14227b3
tsimeonov <tsimeonov12@gmail.com>
2026-08-24 13:33:46 +0200

    update

---
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 4/exercise 4.cpp
@@ -0,0 +1,78 @@
+/*
+The statements in the following program are in incorrect order. Re-arange the statements so that they prompt the user to input the shape type (rectangle, circle, or cylinder) and the appropriate dimension of the shape. The program then outputs the following information about the shape: For a rectangle, it outputs the area and perimeter;
+for a circle, it outputs the area and circumference; and for a cylinder,
+it outputs the volume and surface area. After rearranging the state-
+ments, your program should be properly indented.
+*/
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+
+ string shape;
+ double height;
+
+ cout << "Enter the shape type: (rectangle, circle, cylinder)";
+ cin >> shape;
+ cout << endl;
+
+ double PI = 3.1416;
+ double lenght;
+ double width;
+ double radius;
+
+ cout << fixed << showpoint << setprecision(2);
+
+ if (shape == "rectangle")
+ {
+  cout << "Enter the length of the rectangle ";
+  cin >> lenght;
+  cout << endl;
+
+  cout << "Enter the width of the rectangle ";
+  cin >> width;
+  cout << endl;
+
+  cout << "Perimeter of the rectangle = " << 2 * (lenght + width) << endl;
+
+  cout << "Area of the rectange = " << lenght * width << endl;
+ }
+ else if (shape == "circle")
+ {
+  cout << "Enter the radius of the circle: ";
+  cin >> radius;
+  cout << endl;
+
+  cout << "Circumference of tthe circle: " << 2 * PI * radius << endl;
+
+  cout << "Area of the circle = " << PI * pow(radius, 2.0) << endl;
+ }
+ else if (shape == "cylinder")
+ {
+  cout << "Enter the radius of the base of the cylinder: ";
+  cin >> radius;
+  cout << endl;
+
+  cout << "Enter the height of teh cylinder";
+  cin >> height;
+  cout << endl;
+
+  cout << "Volume of the cylinder = " << PI * pow(radius, 2.0) * height << endl;
+
+  cout << "Surface area of the cylinder"
+       << 2 * PI * radius * height + 2 * PI * pow(radius, 2.0)
+       << endl;
+ }
+ else
+ {
+  cout << "The program does not handle " << shape << endl;
+ }
+
+ return 0;
+}
