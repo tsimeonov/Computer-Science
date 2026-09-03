18db5e3de6fd7b6af3c179991ccecc169e17c157
tsimeonov <tsimeonov12@gmail.com>
2026-09-03 17:16:45 +0200

    update

---
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Chapter 5/example 5-6.cpp
@@ -0,0 +1,59 @@
+/*
+Flag-controlled while loop
+Number guessing game
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+ // Declare the variables
+ // Variable to store the random number
+ int num;
+ // Variable to store the number guessed by the user
+ int guess;
+ // boolean variable to control the loop
+ bool isGuessed;
+
+ srand(time(0));
+
+ num = rand() % 100;
+ cout << "The NUM is " << num << endl;
+
+ isGuessed = false;
+
+ while (!isGuessed)
+ {
+  cout << "Enter an integer greater"
+       << " than or equal to 0 and"
+       << "less than 100: ";
+
+  cin >> guess;
+  cout << endl;
+
+  if (guess == num)
+  {
+   cout << "You guessed the correct "
+        << "number." << endl;
+   isGuessed = true;
+  }
+  else if (guess < num)
+  {
+   cout << "Your guess is lower than the "
+        << "number. \n Guess again!"
+        << endl;
+  }
+  else
+  {
+   cout << "Your guess is higher than "
+        << "the number. \n Guess again!"
+        << endl;
+  }
+ }
+
+ return 0;
+}
