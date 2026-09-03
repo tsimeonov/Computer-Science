caf5a1bccf09372ee44db6adbbbaa2a4b255a0d9
tsimeonov <tsimeonov12@gmail.com>
2026-09-03 15:59:30 +0200

    Update example 5-5.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Chapter 5/example 5-5.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Chapter 5/example 5-5.cpp
@@ -25,6 +25,38 @@ int main()
 
   while (letter != '#')
   {
+    cout << "Letter: " << letter;
+    cout << ", Corresponding telephone digit: ";
+
+    num = static_cast<int>(letter) - static_cast<int>('A');
+
+    if (num <= num && num < 26)
+    {
+      digit = (num / 3) + 2;
+
+      if (((num / 3 == 6) || (num / 3 == 7)) && (num % 3 == 0))
+      {
+        digit = digit - 1;
+      }
+      if (digit > 9)
+      {
+        digit = 9;
+      }
+      cout << digit << endl;
+    }
+    else
+    {
+      cout << "Invalid input" << endl;
+    }
+    cout << "\nEnter another uppercase "
+         << "letter to find its corresponding "
+         << "telephone digit." << endl;
+    cout << "To stop the progrom enter #."
+         << endl;
+
+    cout << "Enter a letter: ";
+    cin >> letter;
+    cout << endl;
   }
 
   return 0;
