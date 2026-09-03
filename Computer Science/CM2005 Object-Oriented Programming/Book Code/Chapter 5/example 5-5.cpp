5c4506d45119578f23fd799f75596d38e7c7bcd0
tsimeonov <tsimeonov12@gmail.com>
2026-09-03 16:23:05 +0200

    Update example 5-5.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Chapter 5/example 5-5.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Chapter 5/example 5-5.cpp
@@ -10,6 +10,41 @@ using namespace std;
 
 int main()
 {
+  char letter;
+  int digit, num;
+
+  cout << "Program to convert uppercase letters to "
+       << "their corresponsding telephone digits."
+       << endl;
+
+  cout << "To stop the program enter #" << endl;
+
+  cout << "Enter an uppercase letter: ";
+  cin >> letter;
+  cout << endl;
+
+  while (letter != '#')
+  {
+    cout << "Letter: " << letter;
+    cout << ", Corresponding telephone digit: ";
+
+    num = static_cast<int>(letter) - static_cast<int>('A');
+
+    if (0 <= num && num < 26)
+    {
+      digit = (num / 3) + 2;
+
+      if ((num / 3 = 6 || (num / 3 == 7) && (num % 3 == 0)))
+      {
+        digit = digit - 1;
+      }
+      if (digit > 9)
+      {
+        digit = 9;
+      }
+      cout << digit << endl;
+    }
+  }
 
   return 0;
 }
