d4b199965058c9d7853ae4498cdc231cbbc0242a
tsimeonov <tsimeonov12@gmail.com>
2026-09-06 13:24:05 +0200

    update

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Chapter 5/example 5-6.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Chapter 5/example 5-6.cpp
@@ -59,5 +59,17 @@ int main()
 }
 
 /*
+logic circuit is an interconnected network of logic gates that process binary signals to perform automated decisions nad processes
 
+logic circuit is an interconnected network of logic gates tha process binary signals to perform automated decisions and processes
+
+logic circuit is an interconnected network of logical gates tha process binary signals to perfrom automated decisions and computations
+
+logic circuit is an interconnected network of logical gates that process binaty signals to perform automated decisions and computations
+
+logic circuit is an interconnected network of logical gates that process binary signals to perform automated decisions and computations
+
+logic gates is an interconnected netwrok og logical gates that process binary signals to perform automated decisions and computations
+
+logic gates is an interconnected network of logical gates that process binary signals to perform automated decions and computation
 */
