c7814fc24e2505c515f3026dccfdd947ed194193
tsimeonov <tsimeonov12@gmail.com>
2026-09-10 12:49:28 +0200

    update

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 4/exercise 16.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 4/exercise 16.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -22,9 +23,54 @@ int main()
  cin >> netPrice;
  cout << endl;
 
- cout << "Enter the estimated number of copies to be solde: ";
+ cout << "Enter the estimated number of copies to be sold: ";
  cin >> copiesSold;
  cout << endl;
+
+ // Calculations
+ royalty1 = OPTION_1_FIXED;
+
+ royalty2 = netPrice * copiesSold * OPTION_2_RATE;
+
+ if (copiesSold <= OPTION_3_THRESHOLD)
+ {
+  royalty3 = netPrice * copiesSold * OPTION_3_RATE__TIER_1;
+ }
+ else
+ {
+  double tier_1_Earnings = netPrice * OPTION_3_THRESHOLD * OPTION_3_RATE__TIER_1;
+  double tier_2_Earnings = netPrice * (copiesSold - OPTION_3_THRESHOLD) * OPTION_3_RATE_TIER_2;
+  royalty3 = tier_1_Earnings + tier_2_Earnings;
+ }
+
+ cout << fixed << setprecision(2);
+
+ cout << "---- OPTION 1 ----" << endl;
+ cout << "Total royalties: $" << royalty1 << endl;
+ cout << "--------------" << endl;
+
+ cout << "---- OPTION 2 ----" << endl;
+ cout << "Total royalties: $" << royalty2 << endl;
+ cout << "--------------" << endl;
+
+ cout << "---- OPTION 3 ----" << endl;
+ cout << "Total royalties: $" << royalty3 << endl;
+ cout << "--------------" << endl;
+
+ // Determine the best option
+ cout << "---- BEST OPTION ----" << endl;
+ if (royalty1 >= royalty2 && royalty1 >= royalty3)
+ {
+  cout << "Options 1 is the most profitable choice." << endl;
+ }
+ else if (royalty2 >= royalty1 && royalty2 >= royalty3)
+ {
+  cout << "Options 2 is the most profitable choice." << endl;
+ }
+ else
+ {
+  cout << "Options 3 is the most profitable choice." << endl;
+ }
 
  return 0;
 }
