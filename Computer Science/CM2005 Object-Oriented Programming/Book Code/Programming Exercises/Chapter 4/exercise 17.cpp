e0b5c7eb9f9a8ff49d4eda15baabc86c742e8c68
tsimeonov <tsimeonov12@gmail.com>
2026-09-12 15:45:11 +0200

    update

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 4/exercise 17.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 4/exercise 17.cpp
@@ -5,23 +5,43 @@ using namespace std;
 int main()
 {
  // Varialbes
- double basePrice;
- double area; // in square feet
- double total;
-
- // calculations
+ double basePriceColonial, areaColonoal, sqColonial;
+ double basePriceSplit, areaSplit, sqSplit;
+ double basePriceSingle, areaSingle, sqSingle;
 
  // Ask for input
- cout << "What is the base price of the house: ";
- cin >> basePrice;
+ cout << "What are the prices for colonial, split-entry and single story: ";
+ cin >> basePriceColonial >> basePriceSplit >> basePriceSingle;
  cout << endl;
 
- cout << "What is the area in square feet: ";
- cin >> area;
+ cout << "What is the area for colonial, split-entry and single story: ";
+ cin >> areaColonoal >> areaSplit >> areaSingle;
  cout << endl;
 
- if ()
+ // calculations
+ sqColonial = basePriceColonial / areaColonoal;
+ sqSplit = basePriceSplit / areaSplit;
+ sqSingle = basePriceSingle / areaSingle;
+
+ // sqColonial = 1000;
+ // sqSplit = 1500;
+ // sqSingle = 500;
+
+ cout << "sq colonial: " << sqColonial << endl;
+ cout << "sq Split: " << sqSplit << endl;
+ cout << "sq Single: " << sqSingle << endl;
+
+ if (sqColonial <= sqSplit && sqColonial <= sqSingle)
  {
+  cout << "Colonial is the cheapest: " << sqColonial << endl;
+ }
+ if (sqSplit <= sqColonial && sqSplit <= sqSingle)
+ {
+  cout << "Split-entry is the cheapest: " << sqSplit << endl;
+ }
+ if (sqSingle <= sqColonial && sqSingle <= sqSplit)
+ {
+  cout << "Single-story is the cheapest: " << sqSingle << endl;
  }
 
  return 0;
