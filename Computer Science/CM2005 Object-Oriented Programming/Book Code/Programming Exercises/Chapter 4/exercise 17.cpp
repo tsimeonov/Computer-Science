55bfd28e233f7c04ad3b4e0bb959a4fa483c9517
tsimeonov <tsimeonov12@gmail.com>
2026-09-12 15:14:50 +0200

    Create exercise 17.cpp

---
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 4/exercise 17.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ // Varialbes
+ double basePrice;
+ double area; // in square feet
+ double total;
+
+ // calculations
+
+ // Ask for input
+ cout << "What is the base price of the house: ";
+ cin >> basePrice;
+ cout << endl;
+
+ cout << "What is the area in square feet: ";
+ cin >> area;
+ cout << endl;
+
+ if ()
+ {
+ }
+
+ return 0;
+}
