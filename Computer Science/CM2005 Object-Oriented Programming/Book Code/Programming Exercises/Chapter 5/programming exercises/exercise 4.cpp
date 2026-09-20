78913ac4f5471115dc2bf1890f84f091d5069bb4
tsimeonov <tsimeonov12@gmail.com>
2026-09-20 13:26:15 +0200

    Update exercise 4.cpp

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 5/programming exercises/exercise 4.cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/Chapter 5/programming exercises/exercise 4.cpp
@@ -13,27 +13,5 @@ int main()
  cin >> letters;
  cout << endl;
 
- cout << "Letters: " << letters << endl;
-
- // Determine how many letters to process (max 7)
- int limit = letters.size();
- if (limit > 7)
- {
-  limit = 7;
- }
-
- for (int i = 0; i < limit; i++)
- {
-  cout << letters[i];
-
-  // add a hyphen after every 3rd word. but not at the end of the printed woed
-  if ((i + 1) % 3 == 0 && i != limit - 1)
-  {
-   cout << "-";
-  }
- }
-
- cout << endl;
-
  return 0;
 }
