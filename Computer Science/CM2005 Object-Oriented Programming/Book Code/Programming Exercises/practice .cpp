32f795424f518f7d6fac2873747bcbfb5bc84340
tsimeonov <tsimeonov12@gmail.com>
2026-09-21 08:53:35 +0200

    clear

--- Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/practice .cpp
+++ Computer Science/CM2005 Object-Oriented Programming/Book Code/Programming Exercises/practice .cpp
@@ -5,21 +5,18 @@ using namespace std;
 
 int main()
 {
-
  int num1, num2;
 
- cout << "Enter two integers: ";
+ cout << "Enter two numbers: ";
  cin >> num1 >> num2;
  cout << endl;
 
- // Determine the lower and upper bonds of the range
  int start = min(num1, num2);
  int end = max(num1, num2);
 
  int count3 = 0;
  int count5 = 0;
 
- // Loopf through all numbers in the range
  for (int i = start; i <= end; i++)
  {
   if (i % 3 == 0)
@@ -32,8 +29,8 @@ int main()
   }
  }
 
- cout << count3 << " numbers are multiple of 3" << endl;
- cout << count5 << " numbers are multiple of 5" << endl;
+ cout << count3 << " numbers are multiples of 3" << endl;
+ cout << count5 << " numbers are multiples of 5" << endl;
 
  return 0;
 }
